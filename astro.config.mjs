b10ff8027902bc6d523d41172ab188ed0253e3db
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-19 11:56:00 +0000

    feat: Add astro-icon integration for lazy loading images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cbf26788cc39e91a2cdbf6d6a514acdf1d7e14ea6f19b11e2571ea5864a33e7
-size 206
+oid sha256:59942629fcf43e6b938379f74fec277a66d244feef64652a4efbaedca73b1121
+size 245
