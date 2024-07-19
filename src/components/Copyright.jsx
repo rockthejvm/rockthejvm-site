24eb66d791c7744d8f634848f9051ed1df40602c
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-19 14:46:58 +0000

    refactor: Simplify Copyright component

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,22 +1,8 @@
 import { h } from "preact";
-import { useState, useEffect } from "preact/hooks";
 
 const Copyright = () => {
-  const [currentYear, setCurrentYear] = useState("");
-
-  useEffect(() => {
-    setCurrentYear(new Date().getFullYear().toString());
-  }, []);
-
-  const style = {
-    top: "100vh",
-  };
-
-  return (
-    <small style={style}>
-      Copyright &copy; {currentYear} | All rights reserved
-    </small>
-  );
+  const currentYear = new Date().getFullYear();
+  return <small>Copyright &copy; {currentYear}</small>;
 };
 
 export default Copyright;
