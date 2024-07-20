30b432a4cd45794d36d78c7419cc91580628df3e
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-20 17:08:50 +0000

    feat: Add lazy loading for images in Contact and Reviews components

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af3284060984fa80ca4700f6b254d48dda4c069df2336fcadb25c516a25e3fd2
-size 2759
+oid sha256:6b3b9f4e3366f5bc2209a3f35d040cfc13b22acf55208e11e0a63c320b449a74
+size 2651
