119bdc366329511d47767967ef56407e5fcdf2da
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-20 00:17:20 +0000

    feat: Add lazy loading for images in LatestContent.astro component

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -2,7 +2,7 @@ import { h } from "preact";
 
 const Copyright = () => {
   const currentYear = new Date().getFullYear();
-  return <small>Copyright &copy; {currentYear}</small>;
+  return <small>&copy; {currentYear} Rock the JVM. All rights reserved.</small>;
 };
 
 export default Copyright;
