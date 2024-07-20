b45bb9b4c7e72a8d878cf27b643def838721a6e2
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-20 00:17:20 +0000

    feat: Add lazy loading for images in LatestContent.astro component

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,8 @@ import { getCollection } from "astro:content";
 export async function GET(context) {
   const posts = await getCollection("posts");
   return rss({
-    title: "Astro Learner | Blog",
-    description: "My journey learning Astro",
+    title: "Astro Learner | Blog", // TODO
+    description: "My journey learning Astro", // TODO
     site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
