9b9118e5749df56e728063c2c2fcd7ba34230fab
wkenned1 <williamvkennedyy@gmail.com>
2024-07-20 21:31:37 +0000

    .
    Co-authored-by: Andrei Liviu Georgescu <andreilgeorgescu@users.noreply.github.com>

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const posts = await getCollection("posts");
+  const posts = await getCollection("blog");
   return rss({
     title: "Astro Learner | Blog", // TODO
     description: "My journey learning Astro", // TODO
