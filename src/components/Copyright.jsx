2893ead0cdbf56b44360b2a1246198d60530db0d
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-23 00:57:01 +0000

    feat: Refactor Copyright component to use TypeScript
    The Copyright component has been refactored to use TypeScript instead of Preact. This change improves type safety and maintainability of the codebase.
    
    Co-authored-by: Andrei Liviu Georgescu <andreilgeorgescu@users.noreply.github.com>

--- src/components/Copyright.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:34ecccf8b0ccfca1e1e2feac9b674f5e024b5d1b8047e2d6995d36a6ee2e20fc
-size 214
---
+++ src/components/Copyright.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:443b8d5688b7ef5059617b7f126dd2d6dd38f4d6fa829c15339cc8a432122c84
+size 106
