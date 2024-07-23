533a7a46e6366fd62cad8b2bb49af72438fcf24a
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-23 18:22:55 +0000

    feat: Refactor config.ts to use separate schema files for collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79677804bf4a4cd6465265a53be339daf181cfb47d9884d9be0a4793527aa4ae
-size 283
+oid sha256:605fe7a97d2d3a489dd8086f3d477b6c9a28190307d4276ca474f91f1796a307
+size 295
