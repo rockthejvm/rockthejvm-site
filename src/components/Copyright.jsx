33577ac32de14701bee0610d7cbc12278fa9101d
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-23 00:57:01 +0000

    feat: Refactor Copyright component to use TypeScript
    The Copyright component has been refactored to use TypeScript instead of Preact. This change improves type safety and maintainability of the codebase.
    
    Co-authored-by: Andrei Liviu Georgescu <andreilgeorgescu@users.noreply.github.com>

--- src/components/Copyright.jsx
+++
@@ -1,8 +0,0 @@
-import { h } from "preact";
-
-const Copyright = () => {
-  const currentYear = new Date().getFullYear();
-  return <small>&copy; {currentYear} Rock the JVM. All rights reserved.</small>;
-};
-
-export default Copyright;
