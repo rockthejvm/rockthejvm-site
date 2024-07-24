5ac31d96742bc40e9f5b8ada1c7461c8fba2d1a6
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-24 20:30:24 +0000

    feat: Implement lazy loading for images in multiple components

--- src/schemas/blog.ts
+++ src/schemas/blog.ts
@@ -6,14 +6,20 @@ export default defineCollection({
     z
       .object({
         authors: reference("authors").default("daniel-ciocirlan"),
-        description: z.string().optional(),
+        description: z
+          .string()
+          .max(200, "Description must be at most 200 characters")
+          .optional(),
         excerpt: z.string(),
         heroImage: image().optional(),
         publishedDate: z.date(),
         series: reference("seriess").optional(),
         summary: z.string().optional(),
         tags: z.array(z.string()),
-        title: z.string(),
+        title: z
+          .string()
+          .min(30, "Title must be at least 30 characters")
+          .max(70, "Title must be at most 70 characters"),
         updatedDate: z.date().optional(),
         video: z.string().url().optional(),
       })
