e1a8995fb52e655b2bd07a5df03f59941728e082
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-24 20:30:24 +0000

    feat: Implement lazy loading for images in multiple components

--- src/schemas/blog.ts
+++ src/schemas/blog.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8271d0eb4e2b91c7c793bf524f5ed01e17b25b7234a0506f33cc053fbbc57f4f
-size 644
+oid sha256:bbd0b5e1909d22fa3b9cc6e74f6d58b6181558d8a6eca5cf64bbcb15d5e72c68
+size 860
