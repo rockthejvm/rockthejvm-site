2ec2a07595a180546b5807e941d400115a9079d2
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-27 04:13:26 +0000

    refactor: Update Hamburger.astro and Header.astro
    The Hamburger.astro file has been modified to add the "type" attribute to the button element, ensuring it is semantically correct.
    
    The Header.astro file has been modified to comment out the ThemeIcon and LoginButton components temporarily.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rockthejvm.com",
+  site: "https://www.rockthejvm.com",
   integrations: [preact(), icon()],
 });
