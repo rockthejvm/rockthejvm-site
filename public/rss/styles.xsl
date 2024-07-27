aa7859cac6085970fcadcaf0b51e2458db401941
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-27 04:13:26 +0000

    refactor: Update Hamburger.astro and Header.astro
    The Hamburger.astro file has been modified to add the "type" attribute to the button element, ensuring it is semantically correct.
    
    The Header.astro file has been modified to comment out the ThemeIcon and LoginButton components temporarily.

---
+++ public/rss/styles.xsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6ac03fbeaf54908011ba72988179ee1c077c61bd2d719167caf209a02eb5839
+size 80494
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2705984a853d13c2ab0189e6466cfbc91fcfc58dc11717810e8b6cae61f23ec
-size 543
+oid sha256:c8a543be3b771e6f27385f166524a03438467ee92e446419fae5d7d28f6dea82
+size 585
