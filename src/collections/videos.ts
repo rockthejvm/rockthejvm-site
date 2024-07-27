37744aedc16ea46d59d1c7788b556cfcc403d6c1
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-27 18:07:44 +0000

    ```text refactor: Update tags validation in videos collection
    Update the validation for the 'tags' field in the videos collection. Previously, it was marked as optional, but now it is required. This change ensures that all videos have at least one tag associated with them.

--- src/collections/videos.ts
+++ src/collections/videos.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8efd504cb4f2916ec2a23cd02442e94ff0c4e8ca016190645f179feacb039865
-size 1057
+oid sha256:4385e1311a718c71bece352a7d6a68aa4a4a91a0492c5997d9c86f10b13fb8c6
+size 1046
