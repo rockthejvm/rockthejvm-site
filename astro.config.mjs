e7f3afd1d485c44099c9106b220fed455b2dd9be
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-27 02:03:10 +0000

    refactor: Remove unused code and comments from Landing.astro and index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-import site from "./src/data/site.json";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: `"https://${site.domain}"`,
+  site: "https://rockthejvm.com",
   integrations: [preact(), icon()],
 });
