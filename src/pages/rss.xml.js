c8faf935a70a97710ceb36d5eb7b79cb1080f6fd
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-27 04:13:26 +0000

    refactor: Update Hamburger.astro and Header.astro
    The Hamburger.astro file has been modified to add the "type" attribute to the button element, ensuring it is semantically correct.
    
    The Header.astro file has been modified to comment out the ThemeIcon and LoginButton components temporarily.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,20 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import site from "../data/site.json";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: "Astro Learner | Blog", // TODO
-    description: "My journey learning Astro", // TODO
-    site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.publishedDate,
-      description: post.data.description,
-      link: `/posts/${post.slug}/`,
-    })),
     customData: `<language>en-us</language>`,
+    description: site.description,
+    items: posts.map((post) => ({
+      description: post.data.excerpt,
+      link: `/blog/${post.slug}/`,
+      pubDate: post.data.publishedDate,
+      title: post.data.title,
+    })),
+    site: context.site,
+    stylesheet: "/rss/styles.xsl",
+    title: `The ${site.name} Blog`,
   });
 }
