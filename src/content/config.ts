0058bc3b96cc4bd0ba03468024ccf68780763d0c
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-27 16:36:45 +0000

    refactor: Update font file paths for Montserrat family
    Update the font file paths for the Montserrat font family to include the correct directory structure ("/public/fonts/"). This ensures that the fonts are loaded correctly and improves the website's typography. Co-authored-by: Andrei Liviu Georgescu <andreilgeorgescu@users.noreply.github.com>

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 import authors from "../collections/authors";
 import blog from "../collections/blog";
 import informationals from "../collections/informationals";
+import memberships from "../collections/memberships";
 import reviews from "../collections/reviews";
 import seriess from "../collections/seriess";
 
@@ -8,6 +9,7 @@ export const collections = {
   authors,
   blog,
   informationals,
+  memberships,
   reviews,
   seriess,
 };
