66ee3120261d33cac9033b22c250983ae2e44de6
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-27 23:50:22 +0000

    feat: Add videos collection
    Add a new videos collection to the config file. This collection will store information about videos, allowing the website to display and categorize videos on the frontend.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eff729e9bccfb8d9994c98d8216bf79b773ed2fd7f53a21b8317cbdba6dac77b
-size 644
+oid sha256:08a17dd8f0c3c5aa884e67a51afb0e2d30187e1e05b8a40fa74b7e2380d8445d
+size 698
