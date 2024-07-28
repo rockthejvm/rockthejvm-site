78672eb1693f34c7ac3d57ec68aa1fc6cc1f9902
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-28 13:54:53 +0000

    feat: Configure Markdoc feature and add Lucas Nouguier as an author
    Add markdoc.config.json and markdoc.config.mts files to configure the Markdoc feature, enabling the website to render and manage aside components. Also, add Lucas Nouguier as an author with his bio, location, photo, and social media links.

--- src/collections/authors.ts
+++ src/collections/authors.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60ba131ff0d1a3960845644ed7775953286ad51b4460e14ac7735329cdfddfa3
-size 1142
+oid sha256:5ae9e9d035f0febeb05219be7c38ee3cce52773e9032d0cbedbcaf7d755e0632
+size 1448
