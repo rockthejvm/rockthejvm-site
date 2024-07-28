1c2626ca640d03cab605fa2a8ca62c97ce7e40a8
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-28 12:44:29 +0000

    feat: Add markdoc.config.json and markdoc.config.mts
    Add markdoc.config.json and markdoc.config.mts files to configure the Markdoc feature. These files define the path, schema, and watch settings for the markdoc.config module. This enables the website to use the Markdoc feature for rendering and managing aside components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4bbfd06f3abcb6e3fc7c0cbe20373169bb21be07606da537985e52a0f51f183
-size 270
+oid sha256:9453891977bd4d9ce2be9b7c471aa0fc1e244d17d0a2cc0f46b66b1a4a07395e
+size 321
---
+++ markdoc.config.mts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f7fc96730d3920882596eca9ef7e30de04788ba4a5d18c16a97e6c7186130495
+size 302
