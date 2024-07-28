7cd815ce82cbe5f0c88565ca6869babc9cbe94c1
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-28 12:44:29 +0000

    feat: Add markdoc.config.json and markdoc.config.mts
    Add markdoc.config.json and markdoc.config.mts files to configure the Markdoc feature. These files define the path, schema, and watch settings for the markdoc.config module. This enables the website to use the Markdoc feature for rendering and managing aside components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
 import icon from "astro-icon";
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.rockthejvm.com",
-  integrations: [preact(), icon()],
+  integrations: [preact(), icon(), markdoc()],
   markdown: {},
 });
