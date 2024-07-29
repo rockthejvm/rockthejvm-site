cda844a31fed153c67a85e0fec6433d85d35661b
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-29 11:40:31 +0000

    refactor: Remove unused Membership component and update homepage layout
    Remove the unused Membership component from the project and update the homepage layout in index.astro. This improves code cleanliness and ensures that only relevant components are included in the project.

---
+++ src/components/LatestContent.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eb865d62bce50944e0f708a0f7532d0db857ff7dabec8ecd11642c0289be4f07
+size 3315
---
+++ src/components/Membership.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:36a92eb016b9e93a685e6d00a2df08d6d84c2d22e1b96329957eb5a72e55b13e
+size 2982
