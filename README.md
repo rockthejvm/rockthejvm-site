dbfbab3992581e49b073a6406668c9cd3422353f
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-29 14:04:13 +0000

    refactor: Update file paths and imports for legal pages
    Update the file paths and imports for the legal pages to reflect the new directory structure. This ensures that the legal pages are correctly linked and rendered in the application.

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
+about-us -> about
 akka-essentials -> akka-pekko-essentials
 akka-http -> akka-pekko-http
 akka-streams -> akka-pekko-streams
 kotlin-at-light-speed -> kotlin-light-speed
 kotlin-bundle -> the-kotlin-bundle
+logo-trademarks -> legal/trademarks
+privacy -> legal/privacy-policy
 scala -> scala-essentials
 scala-advanced-old -> advanced-scala-old
 scala-old -> scala-essentials-old
+terms -> legal/terms-of-use
 the-akka-bundle -> the-akka-pekko-bundle
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import authors from "../collections/authors";
 import articles from "../collections/articles";
 import courses from "../collections/courses";
-import informationals from "../collections/informationals";
+import legals from "../collections/legals";
 import memberships from "../collections/memberships";
 import testimonials from "../collections/testimonials";
 import seriess from "../collections/seriess";
@@ -15,7 +15,7 @@ export const collections = {
   categories,
   clients,
   courses,
-  informationals,
+  legals,
   memberships,
   seriess,
   testimonials,
