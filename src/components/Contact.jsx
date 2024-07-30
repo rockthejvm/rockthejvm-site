625137f5783651c3479cd5ea58fb04359e512943
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-30 13:09:06 +0000

    refactor: Update social media icons and URLs

--- src/components/Contact.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:72f82f6f8580b95b1af1b2047527f94f9879aeb9c1360600da4cf4c0c966dc8b
-size 8407
---
+++ src/components/ContactHelper.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:930ae8d951118e17770d57638fc93f571c783510850e038ee88d5a1ab58c7472
+size 8407
