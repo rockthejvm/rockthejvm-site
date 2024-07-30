e1d563bf1d790b87961b2ab64534c35291024e72
wkenned1 <williamvkennedyy@gmail.com>
2024-07-30 03:57:52 +0000

    Fix styles and responsiveness for home page sections.

--- src/components/Membership.jsx
+++ src/components/Membership.jsx
@@ -11,26 +11,28 @@ const benefits = [
 
 export default function Example() {
   return (
-    <div className="bg-gray-900 py-24 sm:py-32">
+    <div className="py-24 sm:py-32">
       <div className="relative isolate">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
+          <div className="mx-auto flex max-w-2xl flex-col gap-16 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
             <img
               alt=""
-              src="https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-              className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
+              src="https://cdn.filestackcontent.com/kKClNQUjSXmLy790EdpB"
+              className="w-full flex-none rounded-2xl shadow-xl lg:max-w-md"
             />
             <div className="w-full flex-auto">
-              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                Join our team
+              <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
+                Now also for teams!
               </h2>
-              <p className="mt-6 text-lg leading-8 text-gray-300">
-                Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
-                magnam voluptatum cupiditate veritatis in accusamus quisquam.
+              <p className="mt-6 text-lg leading-8 text-black">
+                All of Rock the JVM, at your service. Access ~250 hours of the
+                best content you'll ever find, write more code than many write
+                in their careers, and be welcome in a community of driven people
+                like you.
               </p>
               <ul
                 role="list"
-                className="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base leading-7 text-white sm:grid-cols-2"
+                className="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base leading-7 text-black sm:grid-cols-2"
               >
                 {benefits.map((benefit) => (
                   <li key={benefit} className="flex gap-x-3">
@@ -42,12 +44,18 @@ export default function Example() {
                   </li>
                 ))}
               </ul>
-              <div className="mt-10 flex">
+              <div className="mt-10 flex flex-row justify-start gap-5">
                 <a
                   href="#"
                   className="text-sm font-semibold leading-6 text-indigo-400"
                 >
-                  See our job postings <span aria-hidden="true">&rarr;</span>
+                  Learn more
+                </a>
+                <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-indigo-400"
+                >
+                  Check the team pack
                 </a>
               </div>
             </div>
