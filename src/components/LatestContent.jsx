ccb9906a7e29dba4742f8f1fd2943c65fb56267d
wkenned1 <williamvkennedyy@gmail.com>
2024-07-30 03:57:52 +0000

    Fix styles and responsiveness for home page sections.

--- src/components/LatestContent.jsx
+++ src/components/LatestContent.jsx
@@ -22,7 +22,7 @@ const posts = [
 export default function Example() {
   return (
     <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto min-w-80 px-6 lg:px-8">
         <div className="mx-auto max-w-2xl">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             From the blog
