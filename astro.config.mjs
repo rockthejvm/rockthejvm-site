5cfcfd157509d99f1cf055750a4838880890d1c0
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-30 12:45:46 +0000

    refactor: Update social media icons and URLs

--- astro.config.mjs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1e5ea4730b126b338e5108d6f3267ce6164a43f4d564ebda493d1de1e0215d3d
-size 550
---
+++ astro.config.mts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6540b25f4a526d3480ad57495075512fefbfc01795e4eff342fa7bbdd5cb588d
+size 820
