9d890549cea65872b00584bf70ffcfd6fab8bc21
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-07-31 20:14:28 +0000

    refactor: Update social media icons and URLs

--- src/components/AuthorWideHelper.jsx
+++ src/components/AuthorWideHelper.jsx
@@ -1,10 +1,72 @@
 export default function Example(props) {
   return (
-    <div className="sm:flex">
+    <div className="sm:flex mb-2">
       <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">{props.photo}</div>
       <div>
-        <h4 className="text-lg font-bold">{props.name}</h4>
+        <p className="text-lg font-bold">{props.name}</p>
         {props.biography && <p className="mt-1">{props.biography}</p>}
+        <ul role="list" className="mt-6 flex justify-left gap-x-6">
+          {props.socials.facebook && (
+            <li>
+              <a
+                href={`https://www.facebook.com/${props.socials.facebook}`}
+                className="text-gray-400 hover:text-gray-500"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {props.facebook}
+              </a>
+            </li>
+          )}
+          {props.socials.github && (
+            <li>
+              <a
+                href={`https://www.github.com/${props.socials.github}`}
+                className="text-gray-400 hover:text-gray-500"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {props.github}
+              </a>
+            </li>
+          )}
+          {props.socials.linkedin && (
+            <li>
+              <a
+                href={`https://www.linkedin.com/${props.socials.linkedin}`}
+                className="text-gray-400 hover:text-gray-500"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {props.linkedin}
+              </a>
+            </li>
+          )}
+          {props.socials.twitter && (
+            <li>
+              <a
+                href={`https://www.x.com/${props.socials.twitter}`}
+                className="text-gray-400 hover:text-gray-500"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {props.twitter}
+              </a>
+            </li>
+          )}
+          {props.socials.youtube && (
+            <li>
+              <a
+                href={`https://www.youtube.com/${props.socials.youtube}`}
+                className="text-gray-400 hover:text-gray-500"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {props.youtube}
+              </a>
+            </li>
+          )}
+        </ul>
       </div>
     </div>
   );
--- src/components/LatestContentHelper.tsx
+++ src/components/LatestContentHelper.tsx
@@ -1,3 +1,6 @@
+import type { CollectionEntry } from "astro:content";
+import React from "react";
+
 const posts = [
   {
     id: 1,
@@ -19,9 +22,13 @@ const posts = [
   // More posts...
 ];
 
-export default function Example() {
+interface Props {
+  articles: CollectionEntry<"articles">["data"][];
+}
+
+export default function Example({ articles }: Props) {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white">
       <div className="mx-auto min-w-80 px-6 lg:px-8">
         <div className="mx-auto max-w-2xl">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
@@ -31,47 +38,47 @@ export default function Example() {
             Learn how to grow your business with our expert advice.
           </p>
           <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
-            {posts.map((post) => (
+            {articles.map((article) => (
               <article
-                key={post.id}
+                // key={article.id}
                 className="flex max-w-xl flex-col items-start justify-between"
               >
                 <div className="flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.datetime} className="text-gray-500">
-                    {post.date}
-                  </time>
-                  <a
-                    href={post.category.href}
+                  {/* <time dateTime={article.datetime} className="text-gray-500">
+                    {article.date}
+                  </time> */}
+                  {/* <a
+                    href={article.category.href}
                     className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
                   >
-                    {post.category.title}
-                  </a>
+                    {article.category.title}
+                  </a> */}
                 </div>
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <a href={post.href}>
+                    {/* <a href={article.href}>
                       <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
+                      {article.title}
+                    </a> */}
                   </h3>
                   <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
-                    {post.description}
+                    {/* {article.excerpt} */}
                   </p>
                 </div>
                 <div className="relative mt-8 flex items-center gap-x-4">
-                  <img
+                  {/* <img
                     alt=""
-                    src={post.author.imageUrl}
+                    src={article.author.imageUrl}
                     className="h-10 w-10 rounded-full bg-gray-50"
-                  />
+                  /> */}
                   <div className="text-sm leading-6">
                     <p className="font-semibold text-gray-900">
-                      <a href={post.author.href}>
+                      {/* <a href={article.author.href}>
                         <span className="absolute inset-0" />
-                        {post.author.name}
-                      </a>
+                        {article.author.name}
+                      </a> */}
                     </p>
-                    <p className="text-gray-600">{post.author.role}</p>
+                    {/* <p className="text-gray-600">{article.author.role}</p> */}
                   </div>
                 </div>
               </article>
