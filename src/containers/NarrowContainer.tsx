30d572bcca70e8882c8ada70a66f3ad0c551aefd
wkenned1 <williamvkennedyy@gmail.com>
2024-07-31 03:45:51 +0000

    .

--- src/containers/NarrowContainer.tsx
+++ src/containers/NarrowContainer.tsx
@@ -16,7 +16,7 @@ import type { ReactNode } from "react";
 // }
 export default function Example(props: { children: ReactNode }) {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="container mx-auto px-4 sm:px-6 lg:px-0">
       {props.children}
     </div>
   );
