d39c66735302a5166e393299040842846d308b51
wkenned1 <williamvkennedyy@gmail.com>
2024-08-01 03:51:39 +0000

    .

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -10,7 +10,26 @@ export default defineCollection({
           .array(reference("courses"))
           .min(2, "At least 2 courses are required for a bundle")
           .optional(),
+        benefits: z
+          .object({
+            hours: z.number(),
+            linesOfCode: z.number().int(),
+            numExamples: z.number().int(),
+            rest: z.array(z.string()).optional(),
+          })
+          .strict()
+          .optional(),
         description: z.string(),
+        faqs: z
+          .array(
+            z
+              .object({
+                question: z.string(),
+                answer: z.string(),
+              })
+              .strict(),
+          )
+          .optional(),
         grouping: z.object({
           category: reference("categories"),
           ordinal: z.number().int().positive().optional(),
