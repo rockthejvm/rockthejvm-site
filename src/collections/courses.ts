a1a73b5b477b38c2d46e5d42fc8bfe5905337e03
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-01 23:04:23 +0000

    refactor: Update pubDate format in RSS feed

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c29cd898a0c547ef164d69cd3ff4b1bdad336a9f9dbd723689d7aab7a7ecefd1
-size 1407
+oid sha256:fb7ac8c58086d4e944b9a83a6df6bca22baf19c1af5c2297ae99606ff111ed04
+size 1431
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:687f7469c01916c188fa0b3ac013fcca5d167c69e091902d50690eeeb8ac2c78
-size 610
+oid sha256:3adc5788595a46929ecac56597c85f8434d540b2c75ffccc793e0d8464118390
+size 624
