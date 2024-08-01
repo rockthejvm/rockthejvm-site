8adc9e1f85d712b13f53439febf0c4e76018585b
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-01 19:41:51 +0000

    refactor: Update social media icons and URLs

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -6,30 +6,31 @@ export default defineCollection({
     z
       .object({
         archived: z.boolean().default(false),
+        authors: z
+          .array(reference("authors"))
+          .min(1, "At least 1 author is required")
+          .default(["daniel-ciocirlan"]),
         bundledCourses: z
           .array(reference("courses"))
           .min(2, "At least 2 courses are required for a bundle")
           .optional(),
         benefits: z
           .object({
-            hours: z.number(),
-            linesOfCode: z.number().int(),
-            numExamples: z.number().int(),
+            hours: z.number().positive(),
+            linesOfCode: z.number().int().positive(),
+            numExamples: z.number().int().positive(),
             rest: z.array(z.string()).optional(),
           })
-          .strict()
-          .optional(),
+          .strict(),
         description: z.string(),
-        faqs: z
-          .array(
-            z
-              .object({
-                question: z.string(),
-                answer: z.string(),
-              })
-              .strict(),
-          )
-          .optional(),
+        faqs: z.array(
+          z
+            .object({
+              question: z.string(),
+              answer: z.string(),
+            })
+            .strict(),
+        ),
         grouping: z.object({
           category: reference("categories"),
           ordinal: z.number().int().positive().optional(),
@@ -37,6 +38,7 @@ export default defineCollection({
         image: image(),
         minutesRead: z.string().optional(), // automatically added by remark-reading-time
         price: z.number().positive().optional(),
+        purchaseUrl: z.string().url(),
         title: z.string(),
       })
       .strict(),
