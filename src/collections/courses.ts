241e625d242c999d8825ddb9d2f75055b5075a17
wkenned1 <williamvkennedyy@gmail.com>
2024-08-02 05:07:13 +0000

    .

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb7ac8c58086d4e944b9a83a6df6bca22baf19c1af5c2297ae99606ff111ed04
-size 1431
+oid sha256:5bc9ee6bb31ff30d2d6609c587228446a0e6f75b5f1ed7003d604aaaa0cd9c29
+size 2382
