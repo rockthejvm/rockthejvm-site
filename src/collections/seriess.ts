c0445f84fef823e315e42a03ca1f04a1f6f5f002
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-02 23:19:18 +0000

    refactor: Update members property in seriess collection schema

--- src/collections/seriess.ts
+++ src/collections/seriess.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d8a5b6443cb01047ef4389ca57920404f75e4e342863c0801260ccb6bcf04e1
-size 400
+oid sha256:1cd014f7f274db9b8b3cd772bbf139054051fe8e13ec70ddcd798953d7f966bf
+size 448
