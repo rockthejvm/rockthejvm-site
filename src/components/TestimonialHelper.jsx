e0e85c7edad1ea878281678af617e974f26f894b
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-03 13:29:49 +0000

    Update TestimonialHelper component to use `className` instead of `class`

--- src/components/TestimonialHelper.jsx
+++ src/components/TestimonialHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae1f8f0d5a94d91e02afe08e4b0b887425bdc83d566f451529651ded32ab63da
-size 1375
+oid sha256:7815eced497b5c8695ee7650fcc0ab2366b6489ab472facdf42807cfe606b7c4
+size 1379
