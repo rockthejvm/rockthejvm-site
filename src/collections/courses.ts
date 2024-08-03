469a1c8d12cd41e59c4e0aa98c9e8471d8d0a888
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-03 14:21:29 +0000

    Made `excerpt` required for `courses` content collections

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3030f2754dd6bcc37aefdeb1edcc7b2f31f73d888716f683792922ca613dfde
-size 2718
+oid sha256:f7b29acc1866e2ff9eb2c7dc9647fa84a38eeffbc616742c088e2de85a786a4c
+size 2530
