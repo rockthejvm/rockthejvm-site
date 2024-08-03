a4f95e083203da5625a14e8acdfc7d613d644f41
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-03 13:19:07 +0000

    Update CTA component to include description prop

--- src/components/CTAHelper.jsx
+++ src/components/CTAHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a67e4691e365a0e7b1644a536644b69bd27dc94dd6e48e8c032a4a3efc82a0d9
-size 1370
+oid sha256:ead41a8159eedf6b93fa1279135435c8e88359055114c92f6f165dd8380dc85e
+size 1257
