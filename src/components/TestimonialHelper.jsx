78841b7ae5ae2c4e52874b7755be3e0b850023ca
wkenned1 <williamvkennedyy@gmail.com>
2024-08-03 19:02:41 +0000

    .

--- src/components/TestimonialHelper.jsx
+++ src/components/TestimonialHelper.jsx
@@ -20,7 +20,7 @@ export default function Example(props) {
                 height={3}
                 viewBox="0 0 2 2"
                 aria-hidden="true"
-                className="fill-gray-900"
+                className="fill-white"
               >
                 <circle r={1} cx={1} cy={1} />
               </svg>
