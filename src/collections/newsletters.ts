e75e524582f84d629f8984ef717651b29f668459
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-05 21:41:19 +0000

    Replace `news` article category with dedicated newsletter content collection

---
+++ src/collections/newsletters.ts
@@ -0,0 +1,34 @@
+import { defineCollection, reference, z } from "astro:content";
+
+export default defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z
+      .object({
+        articles: z.array(reference("articles")),
+        courses: z.array(reference("courses")),
+        description: z
+          .string()
+          .max(200, "Description must be at most 200 characters"),
+        excerpt: z.string(),
+        heroImage: z
+          .object({
+            image: image()
+              .refine((image) => image.width >= 1200 && image.height >= 630, {
+                message: "Hero image must be at least 1200x630",
+              })
+              .refine((image) => image.width / image.height === 1.91, {
+                message: "Hero image aspect ratio must be 1.91:1",
+              }),
+            alt: z.string(),
+          })
+          .strict(),
+        publishedDate: z.date(),
+        title: z
+          .string()
+          .min(30, "Title must be at least 30 characters")
+          .max(70, "Title must be at most 70 characters"),
+        videos: z.array(reference("videos")),
+      })
+      .strict(),
+});
