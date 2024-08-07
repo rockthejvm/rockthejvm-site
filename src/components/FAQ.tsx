aacd373b9a85d8b786ce0ab5568e2617595c7b41
wkenned1 <williamvkennedyy@gmail.com>
2024-08-07 02:52:57 +0000

    Add theme text and background styles to course page

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -11,17 +11,17 @@ interface Props {
 
 export default function Example({ faqs }: Props) {
   return (
-    <div className="bg-white">
+    <div className="bg-bkg">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
         <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
-          <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">
+          <h2 className="text-2xl font-bold leading-10 tracking-tight text-ccontent">
             Frequently asked questions
           </h2>
           <dl className="mt-10 space-y-6 divide-y divide-gray-900/10">
             {faqs.map((faq) => (
               <Disclosure key={faq.question} as="div" className="pt-6">
                 <dt>
-                  <DisclosureButton className="group flex w-full items-start justify-between text-left text-gray-900">
+                  <DisclosureButton className="group flex w-full items-start justify-between text-left text-content">
                     <span className="text-base font-semibold leading-7">
                       {faq.question}
                     </span>
