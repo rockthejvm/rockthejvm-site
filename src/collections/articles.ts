c1e3ec25d1e69e5e35abc0ec183752fd283d2d8c
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-07 12:55:37 +0000

    Add validation for updated date in articles collection

--- src/collections/articles.ts
+++ src/collections/articles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff5453117f2747bae7f3de33d9ba5d2012010ce1921ee1cf54a2fe76fddee315
-size 1307
+oid sha256:4d5150ad56d96ad4eb44853a55b5505338602d081ee778db4a7bd33489967c25
+size 1504
