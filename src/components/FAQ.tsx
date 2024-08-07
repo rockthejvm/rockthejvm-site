3f39fbbacc4ffb20c9f5ebb762c74f1f6fcd0c5a
wkenned1 <williamvkennedyy@gmail.com>
2024-08-07 02:52:57 +0000

    Add theme text and background styles to course page

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72ca1562edcaf1409df3f0832a7b1ffbafb7192414d6e3f6683a615bb5bd61ed
-size 3781
+oid sha256:79dfcdbcb9dd105253dd732aea8fd27056fa876b42a0294b863d26a071c73ebb
+size 3778
