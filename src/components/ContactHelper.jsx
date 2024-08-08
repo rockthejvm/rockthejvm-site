ccd44f1cf711e004b271139e5ea06fce3d1c8c02
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-08 19:00:02 +0000

    Add privacy policy link to contact form

--- src/components/ContactHelper.jsx
+++ src/components/ContactHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d1280be26d6ed6889463c2e18b959e269b60e303667b6ad02167751c2119ca1
-size 4752
+oid sha256:8a8d9c80716cf3aaaea5fd60de9b35d7cef16f4a8fd79e3c2c0bcb5f25ec8234
+size 4765
