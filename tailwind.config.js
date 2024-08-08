0368f2c16feff6d14893f4247097d1c3a8a583d3
wkenned1 <williamvkennedyy@gmail.com>
2024-08-08 23:45:27 +0000

    Add pulse animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f7a3154f7f61ed78d46d9392a1626a3fa6a5dee09b88cbe3cb95363897badd6
-size 1158
+oid sha256:60e92e4cdeb05aaf1e2b78ce19f9d15882f56bcbb5614ccf64bbce2c0b909473
+size 1258
