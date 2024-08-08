9dca224a5a6b8e58497295f3278cdbf9d35b1e3a
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-08 19:01:11 +0000

    Change `id` of `first-name` to `name` in contact form

--- src/components/ContactHelper.jsx
+++ src/components/ContactHelper.jsx
@@ -35,15 +35,15 @@ export default function Example() {
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div className="sm:col-span-2">
             <label
-              htmlFor="first-name"
+              htmlFor="name"
               className="block text-sm font-semibold leading-6 text-content"
             >
               Name
             </label>
             <div className="mt-2.5">
               <input
-                id="first-name"
-                name="first-name"
+                id="name"
+                name="name"
                 type="text"
                 autoComplete="given-name"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-content shadow-sm ring-1 ring-inset ring-accent-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
