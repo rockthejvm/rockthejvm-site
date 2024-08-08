62b6ef6e956f90e8aa54b9807068ca970d1025d0
wkenned1 <williamvkennedyy@gmail.com>
2024-08-08 02:18:32 +0000

    Add footer text theme

--- src/components/FooterHelper.jsx
+++ src/components/FooterHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f07e361633c73720f5e97e3e0e0acd49cb851c2d6ed7af7c372e39c71b0736c1
-size 11956
+oid sha256:6321aa59ff1438a56c9e2ae065d7144d2bd3babe64fffb911ad34f916937df84
+size 11954
