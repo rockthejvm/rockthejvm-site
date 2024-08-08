aa9615a55f6a58584073bc638deb5e82dc51c696
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-08 18:59:02 +0000

    Removed `Last Name` field from contact form

--- src/components/ContactHelper.jsx
+++ src/components/ContactHelper.jsx
@@ -33,12 +33,12 @@ export default function Example() {
       </div>
       <form action="#" method="POST" className="mx-auto max-w-xl">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-          <div>
+          <div className="sm:col-span-2">
             <label
               htmlFor="first-name"
               className="block text-sm font-semibold leading-6 text-content"
             >
-              First name
+              Name
             </label>
             <div className="mt-2.5">
               <input
@@ -46,23 +46,6 @@ export default function Example() {
                 name="first-name"
                 type="text"
                 autoComplete="given-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-content shadow-sm ring-1 ring-inset ring-accent-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div>
-            <label
-              htmlFor="last-name"
-              className="block text-sm font-semibold leading-6 text-content"
-            >
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                id="last-name"
-                name="last-name"
-                type="text"
-                autoComplete="family-name"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-content shadow-sm ring-1 ring-inset ring-accent-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
               />
             </div>
