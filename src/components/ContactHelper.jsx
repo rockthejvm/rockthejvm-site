a08eb8887ee24ef78eb38ba4866e2a7a70e03518
wkenned1 <williamvkennedyy@gmail.com>
2024-08-09 01:42:45 +0000

    Update color scheme

--- src/components/ContactHelper.jsx
+++ src/components/ContactHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae84a5f938db6758688e2150859ce93cb876b9f6eb5edb0681eca9f756efddba
-size 4747
+oid sha256:c9d4a57eb9015bd84c0bf28f40fb9cdbb3ad37725eb54a5afb851884df65a4c2
+size 4765
--- src/components/MembershipHelper.jsx
+++ src/components/MembershipHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41237d91b408013bca093e4cc5ee0d07cf4aa70df1a21b0cddc0a823af3b4793
-size 1840
+oid sha256:836cec3e2f7d95238cbe320ef5da6273235888b8af4c5e28a8f37aed060f9ac5
+size 1857
