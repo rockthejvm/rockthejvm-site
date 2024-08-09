c8b5803312d9d3e63dc660a8b42e4c96347c5a11
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-09 00:19:30 +0000

    Cleanup course layout

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e60f6a08986748c19053542f8b26ca68692e207f0834b34ccb6ac5134464fa12
-size 2554
+oid sha256:feba9223f8ee7a80183a6404d76a429aac02c204648f656611b2413ae08d8aba
+size 2485
