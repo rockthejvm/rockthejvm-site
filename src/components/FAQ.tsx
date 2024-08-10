0bfca5d512bdefb9fa322d8f13c30d7ccb551cc7
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-10 17:24:49 +0000

    Use `CollectionEntry` for `FAQ` type

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b05800f7dc837de95b74d222b47f5101310c13f859b8c823f458ce4a0239a37
-size 3782
+oid sha256:7c80ca86be0632ae313be7c6c43336c279311b74678dbf2635d8ddd4bc52a824
+size 3794
