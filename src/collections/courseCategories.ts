aeb422d9f1d5e2ac4d6c77bb534b6382acc3c7c1
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-10 17:55:13 +0000

    Add course category colors

--- src/collections/courseCategories.ts
+++ src/collections/courseCategories.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b7932cf2c46ad81c70dad41e2834d2c4a3db8451ab6117921407e8d6f01622b
-size 224
+oid sha256:922e138bc421ef9d1ac4a72dbd37cb0b95e3017dd4eb3e6f839bbdf68894718a
+size 274
