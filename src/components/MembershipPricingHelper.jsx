fd34384d7fc153928e447b0e04a3165fbe23066b
wkenned1 <williamvkennedyy@gmail.com>
2024-08-20 03:40:49 +0000

    Reformat team pricing

--- src/components/MembershipPricingHelper.jsx
+++ src/components/MembershipPricingHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dddb43361e8a16fca971323930e5ddbc0a52923318e6bf7abee0987ea38c324a
-size 19097
+oid sha256:184840dd501b49e9b2ae137bd1196a94ca4b46960ddbe272bf01dbce9b55cd3b
+size 20309
