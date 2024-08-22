7627a2ca6a74ede65ce1a1fa88bd67e16d6d072a
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-22 00:54:53 +0000

    Cleanup

--- src/components/ContactHelper.jsx
+++ src/components/ContactHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9d4a57eb9015bd84c0bf28f40fb9cdbb3ad37725eb54a5afb851884df65a4c2
-size 4765
+oid sha256:a37c36082580472157930f51e902a792c6ead0db41a84641482ec464a485628c
+size 4561
