21849a9e25666add3346e0dfdd6b026655862a08
wkenned1 <williamvkennedyy@gmail.com>
2024-08-22 04:46:48 +0000

    Set h2 backgrounds to secondary

--- src/collections/courseCategories.ts
+++ src/collections/courseCategories.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:922e138bc421ef9d1ac4a72dbd37cb0b95e3017dd4eb3e6f839bbdf68894718a
-size 274
+oid sha256:ed9d0c8d54ec723d92f7060830ad23c7b39d3e7e096b403331e231e0496c5a07
+size 263
