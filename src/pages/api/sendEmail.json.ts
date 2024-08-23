f7ea605dc985443bdbda79421c9bcabeca1f23c9
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-23 00:35:55 +0000

    Setup contact form

---
+++ src/pages/api/sendEmail.json.ts
@@ -0,0 +1,45 @@
+import type { APIRoute } from "astro";
+import { Resend } from "resend";
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const POST: APIRoute = async ({ params, request }) => {
+  const { from, to, subject, html, text } = await request.json();
+
+  if (!from || !to || !subject || !html || !text) {
+    return new Response(null, {
+      status: 404,
+      statusText: "Did not provide all required data",
+    });
+  }
+
+  const send = await resend.emails.send({
+    from,
+    to,
+    subject,
+    html,
+    text,
+  });
+
+  if (send.data) {
+    return new Response(
+      JSON.stringify({
+        message: send.data,
+      }),
+      {
+        status: 200,
+        statusText: "OK",
+      },
+    );
+  } else {
+    return new Response(
+      JSON.stringify({
+        message: send.error,
+      }),
+      {
+        status: 500,
+        statusText: "Internal Server Error",
+      },
+    );
+  }
+};
