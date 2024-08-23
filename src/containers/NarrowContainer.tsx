a385db12e2ad4d94b03760fed3ed96806ba58c5e
wkenned1 <williamvkennedyy@gmail.com>
2024-08-23 03:28:06 +0000

    Keep background gradient visible with negative z index and remove unnecessary background color classes

--- src/containers/NarrowContainer.tsx
+++ src/containers/NarrowContainer.tsx
@@ -16,7 +16,7 @@ import type { ReactNode } from "react";
 // }
 export default function Example(props: { children: ReactNode }) {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-0">
+    <div className="container mx-auto px-4 sm:px-6 lg:px-0 z-10">
       {props.children}
     </div>
   );
