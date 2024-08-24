ca43a9dc0e3e9e791fc7c43865e3bfe1bd2b0f94
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-24 16:14:10 +0000

    Co-authored-by: wkenned1 <wkenned1@users.noreply.github.com>

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb8ac5948f8b0e4dbe44a3778cec98a44ca0b936099f6741888a3e9b0dbd824d
-size 1911
+oid sha256:1598e27639a149008cb5da4979aa68787b13a8b82ab4998b6497368a7b7316e0
+size 1964
--- src/pages/api/sendEmail.json.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f1522949f40ae97d3c45e30c6619e1bb10faff5b17a7cb84f70846a42daa2e84
-size 905
