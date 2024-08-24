5ba3723c7077641c811ab88bd26a498ef0565c68
wkenned1 <williamvkennedyy@gmail.com>
2024-08-24 01:20:31 +0000

    Add post request for newsletter - Works on postman but not fetch

--- src/components/FooterHelper.jsx
+++ src/components/FooterHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a42ec6f8e6f1dd9f54c7e30496f16d12378f0b2a332f047194798bf14e14b5d2
-size 11971
+oid sha256:c24f608f7d3641e06749bde81eb8ad23ac6e7dc804843592235caf9dd664c4b7
+size 12943
