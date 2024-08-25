63f8aef743cbb718d238465519d64ebf91d57a98
wkenned1 <williamvkennedyy@gmail.com>
2024-08-25 23:13:37 +0000

    Add free check

--- src/components/CoursePrice.jsx
+++ src/components/CoursePrice.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13963d73232b71f4e682e9091569da886a60ef2e1ad9a6bd4005404b110f30ee
-size 801
+oid sha256:cfda7af192bb705430a94a78e76b97326ab91c8c05f6913a15698f80f22df79f
+size 828
