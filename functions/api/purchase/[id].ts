064a45dd636bd3afe32f8b467cea6607255375cc
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-25 17:30:06 +0000

    Co-authored-by: wkenned1 <wkenned1@users.noreply.github.com>

--- functions/api/purchase/[id].ts
+++ functions/api/purchase/[id].ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b1aa9053d1adb28367b6f12779f681e159869adabfdfdcde727a36e74e8ced4
-size 3194
+oid sha256:60265ed8d8f0d01a08d50cc0f39b8f38d41426c38ad41982cb527cee0cc3799b
+size 3812
---
+++ src/utils/sendTeachableRequest.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee1df1d8bb2496c4c7fe4e028b72162c3691f35dbe05a7190152dbd927f7e930
+size 404
