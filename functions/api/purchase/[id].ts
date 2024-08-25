3c7fee0cad723c7a11b0558377067943315bca06
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-25 18:06:11 +0000

    refactor: Improve efficiency of lecture retrieval in purchase API

--- functions/api/purchase/[id].ts
+++ functions/api/purchase/[id].ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60265ed8d8f0d01a08d50cc0f39b8f38d41426c38ad41982cb527cee0cc3799b
-size 3812
+oid sha256:fbf19e8fee27a36185ca9d7c51693a003a842b8b99047a86c321168f28ae1144
+size 3888
