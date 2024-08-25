eda9cb5238c875db6a451073d40bf4741f807dc5
wkenned1 <williamvkennedyy@gmail.com>
2024-08-25 13:41:53 +0000

    Add current price component - in progress
    Co-authored-by: Andrei Liviu Georgescu <andreilgeorgescu@users.noreply.github.com>

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:801fc2ee210e9300cd35332f6d0eaef0f681cae136473c03395486eea2e313b9
-size 1967
+oid sha256:4bc8daac95772bada226ebce2eab3e415bbf4d3f023b18f953e110b35b59b516
+size 2024
