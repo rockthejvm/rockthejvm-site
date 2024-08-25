fb43208e60cd6bcb13d548c77c003e0964df9cae
wkenned1 <williamvkennedyy@gmail.com>
2024-08-25 01:35:36 +0000

    Add error handling

--- functions/api/purchase/[id].ts
+++ functions/api/purchase/[id].ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae2416aba02a4e00a4a35399243e8e75aefdb3538bb4ff8e147d6385c731616f
-size 2854
+oid sha256:2abde57f6e6fa3c009eddad9d3ae0e4de0c1f2718f5686e8fdd479d8429976f1
+size 2947
