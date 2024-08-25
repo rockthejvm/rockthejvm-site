4e848a0af3e84c9fe7fa4719f4737ee97d176b09
wkenned1 <williamvkennedyy@gmail.com>
2024-08-25 21:23:45 +0000

    Fix typo

--- src/components/CourseCurriculumHelper.jsx
+++ src/components/CourseCurriculumHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ed58fce5a929c6859823cc851dd997b1064ad1fe336dfa17096c8a8469495d2
-size 1063
+oid sha256:e2900a587a7bbb2a8d4d5c2734b32a6311bfd8d1b763bf7b876a5dbb53955d96
+size 1065
