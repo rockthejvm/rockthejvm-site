e14fe0dbc05ffcb72243ccf773064554463354a0
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-25 18:11:01 +0000

    refactor: Simplify lecture retrieval in purchase API

--- functions/api/purchase/[id].ts
+++ functions/api/purchase/[id].ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbf19e8fee27a36185ca9d7c51693a003a842b8b99047a86c321168f28ae1144
-size 3888
+oid sha256:c51de6f4a192df15b82048186c1a4a967d31acf7295eef97e7d97ecabf25517b
+size 4002
