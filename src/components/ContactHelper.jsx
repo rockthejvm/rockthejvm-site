29477f9026ec65b01d70410853cfc40a9e325e9e
wkenned1 <williamvkennedyy@gmail.com>
2024-08-26 14:02:47 +0000

    Add privacy policy check on contact form

--- src/components/ContactHelper.jsx
+++ src/components/ContactHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed142a8ddf441d3d8b2cd59648a38baa50c4ebeefcfd51d5c7434e4ebacc2b85
-size 10006
+oid sha256:9c4df78a8482924f33b2a5ea76c379695cbe9b563e9af2dd94e4d2f54fd53503
+size 6159
