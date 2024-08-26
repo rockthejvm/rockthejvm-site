3c59d240015a9baf1be47dba1a452129dcf0e2ff
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-08-26 01:04:43 +0000

    Add `pricingPlanId` to every relevant course

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bc8daac95772bada226ebce2eab3e415bbf4d3f023b18f953e110b35b59b516
-size 2024
+oid sha256:c74260cde0ef3e403f6ff61f7d8cc91b33036c73ae3da6fda6e63d3e07d5b26c
+size 1980
