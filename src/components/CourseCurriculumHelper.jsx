33416a77a33813b08b04261a5e300515037b77d4
wkenned1 <williamvkennedyy@gmail.com>
2024-08-26 04:56:21 +0000

    Update url

--- src/components/CourseCurriculumHelper.jsx
+++ src/components/CourseCurriculumHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a03b99d5d9fe0e2a3c0f693ac818e5efc6bef8aacb32f283ad4d6ee1f52eedb
-size 5185
+oid sha256:fd776b589b8c81871f81f062048f69b0f144c20d8e183ff1565cad28d93ab774
+size 5157
