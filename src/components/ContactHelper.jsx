439190108c5eba2961da424ae4806f4703fbc7b5
wkenned1 <williamvkennedyy@gmail.com>
2024-08-26 23:07:28 +0000

    Remove hardcoded colors

--- src/components/ContactHelper.jsx
+++ src/components/ContactHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c4df78a8482924f33b2a5ea76c379695cbe9b563e9af2dd94e4d2f54fd53503
-size 6159
+oid sha256:f3b09363713419d92aa823724f7ff1c522a5bc09728bb024b6950c5c23cc2992
+size 6157
