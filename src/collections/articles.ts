80f995bec92f6675a1f6ca73816afd87eb4a538d
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-02 19:55:21 +0000

    Replace direct YouTube embeds in articles with  frontmatter field

--- src/collections/articles.ts
+++ src/collections/articles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f7e9c878c033385bfb5cf462131b4edb9a3eca0767736ea048c09d86a6ff046
-size 1957
+oid sha256:47e50f452a497373ef4948aafe117f4cca7cb5346badb69eb9048d5d7de4935b
+size 1947
