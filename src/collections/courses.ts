0a7d732c847683466361695dc7e791686694402f
wkenned1 <williamvkennedyy@gmail.com>
2024-09-03 00:12:03 +0000

    Add tech stack and features to typelevel rite of passage page

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:410a49f0bcc7f90a139a6b15503e5bc78eb5465025647b5e7db0711d3cefb0a6
-size 2018
+oid sha256:fd033d31f9311341343db18e40b2069a14284b75d7e687a72c6e700aab93dfc9
+size 2236
