0c20a403488be5886b281619b7d4c0a423a40202
wkenned1 <williamvkennedyy@gmail.com>
2024-09-05 11:49:00 +0000

    Add embedded script for newsletter

--- src/components/FooterHelper.jsx
+++ src/components/FooterHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aedc4af0c15648f308d34a088524af4c230ed00ee6f423fe3ce32a807bfef490
-size 13096
+oid sha256:d031d2b92540fc7fed87e97e1ca035919fc4bace871ab9aafb805f3452197343
+size 13135
