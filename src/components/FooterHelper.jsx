6e3722b7ee5d330ade7fa651d89b72d8f1171c31
wkenned1 <williamvkennedyy@gmail.com>
2024-09-06 11:12:07 +0000

    Small updates

--- src/components/FooterHelper.jsx
+++ src/components/FooterHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d031d2b92540fc7fed87e97e1ca035919fc4bace871ab9aafb805f3452197343
-size 13135
+oid sha256:eaf6e32aec3845065d01de70b59dd1875c12b22af7c26343e09b58e7021b1d8c
+size 13172
