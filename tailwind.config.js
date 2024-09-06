40a6b8dc6c5172366efc1af00472c6eb2951b29e
wkenned1 <williamvkennedyy@gmail.com>
2024-09-06 14:01:25 +0000

    Fix header bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c7ceef3b1579a387ec6228b1cdbe0ba606fbb0a25ebd8bdd14ebdee0b4912ea
-size 1595
+oid sha256:60e92e4cdeb05aaf1e2b78ce19f9d15882f56bcbb5614ccf64bbce2c0b909473
+size 1258
