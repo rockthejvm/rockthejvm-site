45d0be0f5cd3d4886a3068738e0701741a703602
wkenned1 <williamvkennedyy@gmail.com>
2024-09-06 00:26:57 +0000

    Fix latest content card and add badges to TOC

--- src/components/SocialShareHelper.jsx
+++ src/components/SocialShareHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b693b3f38fabef928fd5f6fcd28ba60e5a605d930ce129cfe3db6118b126a94
-size 733
+oid sha256:e9bbe3f089d9ad664d0d1c98739e3f945fbf047e0094fba83d45518222e75149
+size 693
