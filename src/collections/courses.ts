0d1a7dd784917640f929b9a282004d8372200bf6
wkenned1 <williamvkennedyy@gmail.com>
2024-09-08 19:29:26 +0000

    Remove purchase links and prices
    Co-authored-by: Andrei Liviu Georgescu <andreilgeorgescu@users.noreply.github.com>

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bbbd626f8675821d7ea720f4fbc98b506cc09b0ad3dcc5cf39a23c46a1b3fa0
-size 2291
+oid sha256:25b028cd058cf6ab08828ec4cc60b057089e0fbb019dcd5a5408acb604d4a4a1
+size 2196
