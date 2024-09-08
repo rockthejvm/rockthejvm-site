3df40dfa9e2473fdc0f4a3d5d13b9911f25eb6cc
wkenned1 <williamvkennedyy@gmail.com>
2024-09-08 19:46:44 +0000

    Remove team pack link

--- src/components/MembershipHelper.jsx
+++ src/components/MembershipHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:980a5d7332b1fe4cf856ea1b00b654d2b5510a2aa3d2e0b98f060160cd155626
-size 1879
+oid sha256:e3438634f314d326cb527de73fa28ccde77871211d9c143a21db418cb195d144
+size 1630
