066243c4eecdc95ad1be83d7d9900e81303db6c4
wkenned1 <williamvkennedyy@gmail.com>
2024-09-10 11:39:53 +0000

    Fix tag overflow for article and preview

--- src/collections/articles.ts
+++ src/collections/articles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:380aa7e78fcba8eb7ae6b412544e2d53697596f41f78385ca0245f6b1529fb8d
-size 1763
+oid sha256:f10ea9caa452289922abc510270381e67ec46cdf92991916392432626e6d4f9c
+size 1764
