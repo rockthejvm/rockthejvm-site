e04616cd9db2fe4a80b33cfb4f0831d0a2dca71a
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-13 20:59:23 +0000

    Added latest course to HeroHelper.jsx

--- src/components/HeroHelper.jsx
+++ src/components/HeroHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12804182186365fbfb887f16bed4fcb8a04ca425b8f12ea81cc0ba327c9da97e
-size 1422
+oid sha256:c6180e0edab684df0540a5f82ab87a98fcf438a6cce1650f0dc57c8653e0932e
+size 1576
