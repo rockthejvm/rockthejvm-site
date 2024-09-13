625a95b67f6d45a7968a67acbc390864aefd8637
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-13 21:06:52 +0000

    Check that all ZOD objects are using `strict()`

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:745a455b8f5349c3d2b76321a79f21d5f10027d60885d8fedffcc21784f9c8b2
-size 3553
+oid sha256:4392bf87f5de74d328f3ec928556b3c6d5337de1ad406eff96576e1e5d916173
+size 3582
