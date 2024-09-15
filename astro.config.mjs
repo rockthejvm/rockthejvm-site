9f1b4ea3561b495acc8636ffd9109aba838254d3
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-15 16:54:21 +0000

    Format project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,52 +1,52 @@
-import { defineConfig } from "astro/config";
-import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import sectionize from "@hbsnow/rehype-sectionize";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import sectionize from "@hbsnow/rehype-sectionize";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	site: "https://rockthejvm.com",
-	integrations: [
-		icon({
-			include: {
-				"fa6-brands": [
-					"facebook",
-					"github",
-					"linkedin",
-					"x-twitter",
-					"youtube",
-				],
-				"fa6-solid": ["rss", "table-list", "caret-up"],
-				heroicons: ["computer-desktop", "moon", "sun"],
-			},
-		}),
-		mdx(),
-		tailwind({
-			applyBaseStyles: false,
-			nesting: true,
-		}),
-		react(),
-		sitemap(),
-	],
-	markdown: {
-		remarkPlugins: [],
-		rehypePlugins: [sectionize],
-		shikiConfig: {
-			themes: {
-				light: "github-light-default",
-				dark: "github-dark-default",
-			},
-		},
-	},
-	redirects: {
-		"/p/about-us": "/about",
-		"/p/privacy": "/legal/privacy",
-		"/p/terms": "/legal/terms",
-		"/p/logo-trademarks": "/legal/trademarks",
-		"/p/membership": "/memberships/personal",
-		"/p/team-pack": "/memberships/team",
-		"/articles": "/articles/1",
-	},
+  site: "https://rockthejvm.com",
+  integrations: [
+    icon({
+      include: {
+        "fa6-brands": [
+          "facebook",
+          "github",
+          "linkedin",
+          "x-twitter",
+          "youtube",
+        ],
+        "fa6-solid": ["rss", "table-list", "caret-up"],
+        heroicons: ["computer-desktop", "moon", "sun"],
+      },
+    }),
+    mdx(),
+    tailwind({
+      applyBaseStyles: false,
+      nesting: true,
+    }),
+    react(),
+    sitemap(),
+  ],
+  markdown: {
+    remarkPlugins: [],
+    rehypePlugins: [sectionize],
+    shikiConfig: {
+      themes: {
+        light: "github-light-default",
+        dark: "github-dark-default",
+      },
+    },
+  },
+  redirects: {
+    "/p/about-us": "/about",
+    "/p/privacy": "/legal/privacy",
+    "/p/terms": "/legal/terms",
+    "/p/logo-trademarks": "/legal/trademarks",
+    "/p/membership": "/memberships/personal",
+    "/p/team-pack": "/memberships/team",
+    "/articles": "/articles/1",
+  },
 });
--- src/collections/companies.ts
+++ src/collections/companies.ts
@@ -1,12 +1,12 @@
 import { defineCollection, z } from "astro:content";
 
 export default defineCollection({
-	type: "data",
-	schema: z
-		.object({
-			slug: z.string(),
-			title: z.string(),
-			website: z.string().url(),
-		})
-		.strict(),
+  type: "data",
+  schema: z
+    .object({
+      slug: z.string(),
+      title: z.string(),
+      website: z.string().url(),
+    })
+    .strict(),
 });
