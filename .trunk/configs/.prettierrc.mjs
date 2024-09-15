f316b22eb05c76764ba0b09fa60e152fd1547ecc
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-15 16:27:30 +0000

    Fix linting/formatting

---
+++ .trunk/configs/.prettierrc.mjs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ae333cd97a5947fa1ad06758539e575cce84fe17f662ee31bd5f46fa44bfe3ff
+size 512
---
+++ .trunk/configs/eslint.config.mjs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0a993bc64f614a1f4e854db9993b1a13b9ece0752ace04a65e76655a1bbfe5db
+size 793
