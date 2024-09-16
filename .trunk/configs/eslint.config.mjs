1c7faef81b209f33c7bf1c64e89cd5f3b9cf5cf3
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-16 01:13:58 +0000

    Add `globals` package

--- .trunk/configs/eslint.config.mjs
+++ .trunk/configs/eslint.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a993bc64f614a1f4e854db9993b1a13b9ece0752ace04a65e76655a1bbfe5db
-size 793
+oid sha256:f756f14106aeffceea754858016940455ba1f863d386aa57a7f4532f6ba3cb17
+size 933
