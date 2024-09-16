a677b19d1b4da7c266ceadb9e7cdad1135ad81b5
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-16 01:13:58 +0000

    Add `globals` package

--- .trunk/configs/eslint.config.mjs
+++ .trunk/configs/eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -11,6 +12,12 @@ export default tseslint.config(
   ...eslintPluginAstro.configs.recommended,
   ...eslintPluginAstro.configs["jsx-a11y-strict"],
   {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
     },
