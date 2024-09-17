1c743b74c554650e8e9c67d3c51cfdac10bf4f73
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-17 01:35:38 +0000

    Add linting support for mdx, react, and tailwind

--- .trunk/configs/eslint.config.mjs
+++ .trunk/configs/eslint.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f756f14106aeffceea754858016940455ba1f863d386aa57a7f4532f6ba3cb17
-size 933
+oid sha256:836ade5935fc800e257d055614cb896baeaa73968b8514181e50f28072798ed3
+size 1409
