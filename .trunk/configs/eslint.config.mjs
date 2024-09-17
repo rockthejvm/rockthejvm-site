3adfe81455a13bcf7c5ff608ff9aeaa4c29602cb
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-17 01:35:38 +0000

    Add linting support for mdx, react, and tailwind

--- .trunk/configs/eslint.config.mjs
+++ .trunk/configs/eslint.config.mjs
@@ -1,25 +1,48 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import eslintPluginAstro from "eslint-plugin-astro";
+import js from "@eslint/js";
+import astro from "eslint-plugin-astro";
+import * as mdx from "eslint-plugin-mdx";
+import tailwind from "eslint-plugin-tailwindcss";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import ts from "typescript-eslint";
+const react = require("eslint-plugin-react");
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  ...eslintPluginAstro.configs.recommended,
-  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.strict,
+  ...ts.configs.stylistic,
+  react.configs.flat.recommended,
+  ...tailwind.configs["flat/recommended"],
+  ...astro.configs.recommended,
+  ...astro.configs["jsx-a11y-strict"],
   {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
     },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+  {
+    ...mdx.flat,
+    processor: mdx.createRemarkProcessor({
+      lintCodeBlocks: true,
+    }),
+  },
+  {
+    ...mdx.flatCodeBlocks,
+    rules: {
+      ...mdx.flatCodeBlocks.rules,
     },
   },
 );
