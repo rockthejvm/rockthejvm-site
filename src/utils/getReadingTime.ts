fc4e722056d1efa30138248413be099a39831a1d
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-17 03:38:43 +0000

    Fix `getReadingTime.ts` linting issues

--- src/utils/getReadingTime.ts
+++ src/utils/getReadingTime.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1d39cc3494a8ceec845704a83b604e1f8b0233417d1e95513d83b36966ae7ab
-size 497
+oid sha256:f3cd820129d22d4122ceff82b74634be273bfcb66d092bacc6612a3a35654c58
+size 489
