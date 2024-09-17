b218d199c497637ee4ac6f86be3c8df8c1e32c3a
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-17 03:38:14 +0000

    Fix `tailwind.config.js` linting issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecd01cc19ff3cda704c607ef5edc45fc350ccbf4ba9b868ee927fe4faf58f0e8
-size 1314
+oid sha256:2754bd8b70222ba88c3a17b4fe5c57df11aa8b1950c82668779655f915d522ff
+size 1367
