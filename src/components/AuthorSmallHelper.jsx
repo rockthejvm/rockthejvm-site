4f65b20d3eab19c92d3a73784b435cf499313f9e
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-27 02:22:40 +0000

    Fix article layout spacing and structure

--- src/components/AuthorSmallHelper.jsx
+++ src/components/AuthorSmallHelper.jsx
@@ -1,31 +1,16 @@
 export default function Example(props) {
   return (
     <div className="mb-2 mt-6 flex">
+      {/* <a href={`/authors/${props.authorId}`} className="mt-auto"> */}
       <div className="mr-4 shrink-0">{props.photo}</div>
-      <div>
-        <p className="text-lg font-bold text-emerald-500">{props.authorName}</p>
-        <p className="mt-0.5">{props.time}</p>
-        {props.minutesRead && (
-          <p className="mt-0.5 inline-flex items-center text-gray-600">
-            <span className="inline-flex items-center gap-x-1.5 rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="ml-1 size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-              {`${props.minutesRead} min read`}
-            </span>
+      {/* </a> */}
+      <div className="text-content-1">
+        <a href={`/authors/${props.authorId}`} className="mt-auto">
+          <p className="text-lg font-bold text-content-1 hover:text-accent-2 sm:text-xl">
+            {props.authorName}
           </p>
-        )}
+        </a>
+        <p className="mt-0.5">{props.time}</p>
       </div>
     </div>
   );
