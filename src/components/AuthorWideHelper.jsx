dff65884fde033a53a76ab360fab05ab2f07c98e
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-27 04:04:35 +0000

    Fix author wide helper

--- src/components/AuthorWideHelper.jsx
+++ src/components/AuthorWideHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e941df78f956f65b545bfcac84400ad199afca019a8a5484a2f65b538206b36
-size 2465
+oid sha256:4f882282ce69813bfd434c1e4ed3706a11ac9107c2c1443401007281a0dea262
+size 2726
