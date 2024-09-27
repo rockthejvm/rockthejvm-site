57ace4f4df287182e2c3f13164ba8a1ad3c46908
wkenned1 <williamvkennedyy@gmail.com>
2024-09-27 03:50:10 +0000

    Center author text and update size

--- src/components/AuthorWideHelper.jsx
+++ src/components/AuthorWideHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48ff1e0fad475d9b861c9fd549f47fadb84a07148cc124a71f62570fa92d5fd4
-size 2431
+oid sha256:1e941df78f956f65b545bfcac84400ad199afca019a8a5484a2f65b538206b36
+size 2465
