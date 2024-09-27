cd0a70922519a72361a30f4c9e85e514be076783
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-27 05:18:30 +0000

    Make membership pricing cards more consistent

--- src/components/MembershipPricingHelper.tsx
+++ src/components/MembershipPricingHelper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8941685e34ce90af2fadede7b2e61d9ef86b382723850fbf8a2ea399c5cb93e2
-size 22568
+oid sha256:84b1594e4649676001b01c2f562d40a1b985d6e69faee470d1a2a519c2c78609
+size 22862
