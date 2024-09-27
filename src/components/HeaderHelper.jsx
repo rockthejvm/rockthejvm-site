a1c7fe54bbcba4ad2edd817deba70f8a4943f66c
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-27 04:32:42 +0000

    Make mobile hamburger menu use theme colors

--- src/components/HeaderHelper.jsx
+++ src/components/HeaderHelper.jsx
@@ -60,7 +60,7 @@ export default function Example(props) {
         className="lg:hidden"
       >
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-bkg p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">{site.name}</span>
