f5059ea6a60c54f7874317392e1367220a6b2c78
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-09-27 04:32:42 +0000

    Make mobile hamburger menu use theme colors

--- src/components/HeaderHelper.jsx
+++ src/components/HeaderHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad8dab9d566d47b8ac97d69686b2ddf62385f199c2f142fe1d871b660409e35d
-size 3746
+oid sha256:341ddf62473ed7e66308d51213cdffd5dad30b12340f13e64ce0912857b743a8
+size 3744
