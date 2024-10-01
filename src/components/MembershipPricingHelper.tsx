5525823e343e03f34ac9bd5d8b57835fa048895b
wkenned1 <williamvkennedyy@gmail.com>
2024-10-01 23:39:17 +0000

    Add margins to testimonial/social proof sections. Add call to action (cta) color for buttons

--- src/components/MembershipPricingHelper.tsx
+++ src/components/MembershipPricingHelper.tsx
@@ -318,8 +318,7 @@ export default function Example(props) {
               <a
                 href="https://rockthejvm.com/purchase?product_id=4131055"
                 aria-describedby="tier-hobby"
-                // className="mt-8 block rounded-full px-4 py-2.5 text-center text-sm font-semibold text-content ring-1 ring-inset ring-primary hover:ring-accent-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary sm:mt-10"
-                className="mt-8 block rounded-xl bg-primary px-3.5 py-2.5 text-center text-sm font-semibold text-content-1 shadow-sm hover:bg-accent-1 hover:text-content-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary sm:mt-10"
+                className="mt-8 block rounded-xl bg-cta px-3.5 py-2.5 text-center text-sm font-semibold text-content-1 shadow-sm hover:bg-accent-1 hover:text-content-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cta sm:mt-10"
               >
                 Join Now
               </a>
@@ -489,7 +488,7 @@ export default function Example(props) {
               <a
                 href="https://rockthejvm.com/purchase?product_id=4131056"
                 aria-describedby="tier-enterprise"
-                className="mt-8 block rounded-xl bg-primary px-3.5 py-2.5 text-center text-sm font-semibold text-content-1 shadow-sm hover:bg-accent-1 hover:text-content-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary sm:mt-10"
+                className="mt-8 block rounded-xl bg-cta px-3.5 py-2.5 text-center text-sm font-semibold text-content-1 shadow-sm hover:bg-accent-1 hover:text-content-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cta sm:mt-10"
               >
                 Join Now
               </a>
@@ -536,7 +535,7 @@ export default function Example(props) {
           <a
             href="/contact"
             aria-describedby="tier-hobby"
-            className="mt-8 block rounded-full px-4 py-2.5 text-center text-sm font-semibold text-content ring-1 ring-inset ring-primary hover:ring-accent-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary sm:mt-10"
+            className="mt-8 block rounded-full px-4 py-2.5 text-center text-sm font-semibold text-content ring-1 ring-inset ring-cta hover:ring-accent-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cta sm:mt-10"
           >
             Join Now
           </a>
