f97544ea483da0ee8a4a96f13ec1a387f84c3429
wkenned1 <williamvkennedyy@gmail.com>
2024-10-03 01:54:32 +0000

    Fix card shading bug

--- src/components/TestimonialHelper.jsx
+++ src/components/TestimonialHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce922ce015b8bef5d362dfe3f9a92fa0a07873529a5fc2e11d16445be88c4949
-size 1426
+oid sha256:2763ad3504d299f0133cb28af9904d2b81f08f41f8b6f4047cf953a88c0a989c
+size 1430
