c072309731ffa5592b1c62c8e96ff000c0511957
wkenned1 <williamvkennedyy@gmail.com>
2024-10-04 01:29:02 +0000

    Fix curriculum requests

--- src/components/CourseCurriculumHelper.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:08adf94e5346cac689f949a739c0026ef229f4147a6bfda4f57a59d2f8780fe1
-size 5172
---
+++ src/components/CourseCurriculumHelper.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef55391e034e30d56b18dd347bdc19fa2cc49b011a160ec6a570791fde8b94ac
+size 7212
