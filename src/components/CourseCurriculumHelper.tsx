8a3d31e183258cc10e6939bed9f386a17e2d0bd7
wkenned1 <williamvkennedyy@gmail.com>
2024-10-09 23:14:19 +0000

    Add base url to curriculum buttons

--- src/components/CourseCurriculumHelper.tsx
+++ src/components/CourseCurriculumHelper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef55391e034e30d56b18dd347bdc19fa2cc49b011a160ec6a570791fde8b94ac
-size 7212
+oid sha256:bcf9fe898c0fc5f271bb1aa106091f4dcb941dcc31a2d9acd27afa77ae2e3da1
+size 7234
