e6fbb9758e042dc600bbae1def969f225a9902fe
wkenned1 <williamvkennedyy@gmail.com>
2024-10-10 03:19:55 +0000

    Add loading indicator to curriculum section

--- src/components/CourseCurriculumHelper.tsx
+++ src/components/CourseCurriculumHelper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcf9fe898c0fc5f271bb1aa106091f4dcb941dcc31a2d9acd27afa77ae2e3da1
-size 7234
+oid sha256:d4e2aa528b38a2a552151884031e6fd45493d66596a986c5978ef6c528876e81
+size 7498
