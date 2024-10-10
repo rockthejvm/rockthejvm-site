1667c773971474e7ad75593532c7d231acb91218
wkenned1 <williamvkennedyy@gmail.com>
2024-10-10 01:30:54 +0000

    Hide normal TOC for smaller sizes

--- src/pages/articles/_components/FloatingActionMenuHelper.jsx
+++ src/pages/articles/_components/FloatingActionMenuHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2acc0ee1b2791358b65340a6d01c4f32011a7dff9534525baae9bb5d77133ac
-size 1886
+oid sha256:e866a16036549b6d271f36379eb27fc92946b3dcd5696c453e53ebae6d04bb50
+size 1925
