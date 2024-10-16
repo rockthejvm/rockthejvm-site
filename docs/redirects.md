37111f4abfc0ea23167d7d2d2a16ed156e195b60
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-10-16 02:53:08 +0000

    Fix linting and formatting issues

--- docs/redirects.md
+++ docs/redirects.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6ee982bca55f858743058b0108c988a67fbd151975c469a638ac92b4ce51e99
-size 9501
+oid sha256:998ee7f0882584513c04b399407d2439dbd4fb2d7f0303830db17f3ec7e86a59
+size 9512
--- src/components/MembershipPricingHelper.tsx
+++ src/components/MembershipPricingHelper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4889aee2731ffe5a3ce9604b1a6c5ea0782e7ea047fc0a7b88ace5f4de88bebf
-size 22305
+oid sha256:6678d4501ecc0776054f4d0f26153a160928722887cc38184336f0af7cc54106
+size 22365
