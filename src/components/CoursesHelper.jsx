1ec87a029bf3f5be9700433cc67bfd741b453824
wkenned1 <williamvkennedyy@gmail.com>
2024-10-17 22:39:43 +0000

    Add card shadows

--- src/components/CoursesHelper.jsx
+++ src/components/CoursesHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b167533f3a8b45b5ebb358d09045356ccd43cfa95736eea7918651a7f279f8a7
-size 1261
+oid sha256:871cbff145a6db8802366adeac9cb19079888a6476c591cf83694d1ca01dd61e
+size 1291
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9038635dce9c5a01387a3fc2ec3b415e39292d35a8bcbe6fea205459ea9a0d5
-size 1425
+oid sha256:d185d7ef38ea828f275a6deebf6cbacb97fd1f130c18ce6b494ecf76aa8b0d51
+size 1487
