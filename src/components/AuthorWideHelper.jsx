8511c4bbe66fb953175c9558670a67b7d2470b16
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-10-17 23:27:32 +0000

    Add website icon to AuthorWide

--- src/components/AuthorWideHelper.jsx
+++ src/components/AuthorWideHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:140f5dbee7f12e00787f2e53ced48e7d1a1e57a7b659ab31ab09e0c78b62baa0
-size 2721
+oid sha256:a36a992fb53f28b995fe5df59cc61bf59976d0a8a3731b94aa2ccddf628c4eda
+size 3087
