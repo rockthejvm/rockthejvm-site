5ddbec00fbf82e1ef05e5808b4d0aae03af4267e
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2024-10-23 21:31:30 +0300

    course category logos

--- src/collections/courseCategories.ts
+++ src/collections/courseCategories.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:570eff51aef32c8327e46abd00542138d7bf229a74907c1011c51439227086c7
-size 452
+oid sha256:f53380dad18a71802fd22bc8e4a709e49549e6abecf2a0ea5481473c33dc10ca
+size 514
--- src/components/CoursesHelper.jsx
+++ src/components/CoursesHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:871cbff145a6db8802366adeac9cb19079888a6476c591cf83694d1ca01dd61e
-size 1291
+oid sha256:a8b7c8ccffa7bca312c9d1825c701a9bdcc054e8084abb3e718813801c1d9726
+size 1498
