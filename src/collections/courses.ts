e6a99bd36e93f223980b4f4b7d18caba40a62745
wkenned1 <williamvkennedyy@gmail.com>
2024-10-24 02:29:20 +0000

    Fix collaborator and special thanks sections

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:206f5b8dba2a4369591b99ae2e2ae476db5f4fa9da3b3ebd407b67039cc0d003
-size 4466
+oid sha256:0b885b144fc4065517a9249db8bcc8c811dbd8c6c5e367ef5090c4f8dc9472e6
+size 4835
