1caed4e9d80cf6bbe2283fc05fd312147faafd57
wkenned1 <williamvkennedyy@gmail.com>
2024-10-25 04:04:29 +0000

    Change hero breakpoint

--- src/components/HeroHelper.jsx
+++ src/components/HeroHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c546892116312c54ab1aba6623343cfa022da6ace155dd58a0430e4cb977e8f
+oid sha256:7179f98db9a16c07c776e2326ac5b01a0cad903046fca176698cd6ef430e7895
 size 1748
