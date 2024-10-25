761d125f4f8c558e2bebe8cb098b1a9ca7d83b05
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-10-25 13:20:54 +0000

    Make course category mandatory

--- src/collections/courseCategories.ts
+++ src/collections/courseCategories.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f53380dad18a71802fd22bc8e4a709e49549e6abecf2a0ea5481473c33dc10ca
-size 514
+oid sha256:84403431ea17db95045f06b1f51314a9f5c7ab9365d9fb1ec4a51990ad7939f6
+size 476
