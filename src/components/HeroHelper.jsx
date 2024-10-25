9b5390bf17b12107579f92d18488cab914c56fe1
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-10-25 12:50:49 +0000

    Improve new badge message

--- src/components/HeroHelper.jsx
+++ src/components/HeroHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7179f98db9a16c07c776e2326ac5b01a0cad903046fca176698cd6ef430e7895
-size 1748
+oid sha256:ffdd364328d7fa31168683b84922442d6f9e6bc2952a70b77ea20ceccd9fcd2b
+size 1763
