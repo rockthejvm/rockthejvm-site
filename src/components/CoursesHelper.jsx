a784c051c616198ad98996f0298fdb1fd1f9b58f
wkenned1 <williamvkennedyy@gmail.com>
2024-10-25 14:30:24 +0000

    Fix responsiveness for logos

--- src/components/CoursesHelper.jsx
+++ src/components/CoursesHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33ce56fd606983015299efbb7bf0324359f1558e516a1bc66fd7e91c41603fbf
-size 1859
+oid sha256:e7221e496a04363dc3e3fc5dc32dfd064f19ff564b9720449ee4c68d3e8ba35c
+size 1696
