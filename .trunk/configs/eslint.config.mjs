153eb4aaae5a093466af67fe3a70c05c813e3e7a
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-10-28 21:22:50 +0000

    Turn off non-null assertion eslint rule

--- .trunk/configs/eslint.config.mjs
+++ .trunk/configs/eslint.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33d3a94eb360f889f8889cb47447f68d6009c7bff29ce4cc2e7c38798b56cb6c
-size 1664
+oid sha256:136513ed39d3e395db4de6a418ff505293b1189274db232836c76e75d5e6a151
+size 1721
