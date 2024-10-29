f5fbb63cab51f91160201962b2fe89ef4ac78525
wkenned1 <wkenned1@binghamton.edu>
2024-10-29 14:17:38 +0000

    Co-authored-by: Andrei Liviu Georgescu <andreilgeorgescu@users.noreply.github.com>

--- src/components/CourseCurriculumHelper.tsx
+++ src/components/CourseCurriculumHelper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fbdf2a0344e12cb6437abb04d7a1ab93bbc588cd673cd58735f15e96595d11d
-size 8433
+oid sha256:3a02e65d5c3f70ff3b67dde0dd39609c488e87f88bf65b05e2a7aae582c0f2f1
+size 8352
