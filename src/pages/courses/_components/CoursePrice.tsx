f7ac85e100c477d69760ccb83e1c48e174d91783
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-10-29 12:41:39 +0000

    Update cloudflare pages function

--- src/pages/courses/_components/CoursePrice.tsx
+++ src/pages/courses/_components/CoursePrice.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:070ad40fd3b022d4228ce169ae4d05fe787014db5d8b8998488e844592ed6371
-size 2282
+oid sha256:932f49abc43a5e51d759d6276a1c606f4ab8a45e980fcafa2a0c703f4304b0ad
+size 2256
