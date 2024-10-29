4c79dec02d45b100007a0921e7e77b597673f918
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-10-29 01:42:00 +0000

    Fix new course link

--- src/components/HeroHelper.jsx
+++ src/components/HeroHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffdd364328d7fa31168683b84922442d6f9e6bc2952a70b77ea20ceccd9fcd2b
-size 1763
+oid sha256:d97aa4d00091ea448a2478b56173af35d21886dc12b5f3fdc13f27d8cc1bd697
+size 1765
