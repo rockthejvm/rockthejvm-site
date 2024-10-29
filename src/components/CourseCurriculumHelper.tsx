4176536e00178c8cc117f839838c071587419909
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-10-29 13:10:22 +0000

    Fix type errors

--- src/components/CourseCurriculumHelper.tsx
+++ src/components/CourseCurriculumHelper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89392af062c377aa3ebf569eb026afabf33edf379c97f467a443ab274923507a
-size 8282
+oid sha256:6fbdf2a0344e12cb6437abb04d7a1ab93bbc588cd673cd58735f15e96595d11d
+size 8433
