6b494e1d51f92167d292ff0e666de11f72c1a1f2
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-10-29 12:17:35 +0000

    Update cloudflare pages functions

--- functions/api/purchase/[id].ts
+++ functions/api/purchase/[id].ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb07a64b4a4e4f4e88fa406427bbdef55d7dad9444567304c2fde70532d699da
-size 4330
+oid sha256:844ea35b15e53c36fa3c30c3d25f9af69afb0aad68f2e836eab024f2d3ab80f9
+size 3968
