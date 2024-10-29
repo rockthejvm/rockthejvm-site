15b3755ef68a97d0a306f9002b112b8c4f0e1908
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-10-29 12:46:28 +0000

    Update CoursePrice interface

--- src/pages/courses/_components/CoursePrice.tsx
+++ src/pages/courses/_components/CoursePrice.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:932f49abc43a5e51d759d6276a1c606f4ab8a45e980fcafa2a0c703f4304b0ad
-size 2256
+oid sha256:b048b5f992c023dea5807b5534c9d6c6ece3f54d550a5d43445ddebcffcec78b
+size 2363
