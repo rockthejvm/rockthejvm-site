df2d372e748ef23146c75d8f8827a22a21d14344
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-11-01 14:51:54 +0000

    Move feed to articles directory

---
+++ src/pages/articles/feed.xml.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:14053d1b67411e38313fea71eecae794199ba6d01f14a5820a7f809d046ed841
+size 631
--- src/pages/feed.xml.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d30d47e4a6498f65d5cde0d8e884bf42476c56fe69ce33bd54cbff8baca5fc8d
-size 628
