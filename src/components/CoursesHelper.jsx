9900b901e14723183699320bb26d005242cc7eb0
wkenned1 <wkenned1@binghamton.edu>
2024-11-01 14:59:59 +0000

    Add missing alt tags to logos

--- src/components/CoursesHelper.jsx
+++ src/components/CoursesHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7221e496a04363dc3e3fc5dc32dfd064f19ff564b9720449ee4c68d3e8ba35c
-size 1696
+oid sha256:b4bd419f428c3afab5ce882dce463bbe9b91a7cf706dba997e6ac068ad4dc593
+size 1755
