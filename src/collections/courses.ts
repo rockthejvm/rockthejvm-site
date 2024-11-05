1889edb4e1a9183fd95cbd75666b015c3c93343d
Daniel <daniel.ciocirlan@gmail.com>
2024-11-05 16:51:11 +0200

    RoP course page should not have membership (#150)
    * RoP course pages should not have membership

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ded25282fd774094106936cd3530204a12f1ca85bbe6f6d6b5b9bc7024e298ab
-size 4827
+oid sha256:0b31988affe95e97b32faf8fa2d721e464e580c798cef91146d38415a4ea1278
+size 4884
