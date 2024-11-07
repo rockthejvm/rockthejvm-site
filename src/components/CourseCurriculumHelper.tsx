a992a6f52abce63bae5bbfef66f7cb1838f01baa
wkenned1 <wkenned1@binghamton.edu>
2024-11-06 19:41:52 -0500

    Reduce horizontal margin for small widths (#132)
    * Reduce horizontal margin for small widths
    
    * Add margin to curriculum
    
    * Switch margin with padding

--- src/components/CourseCurriculumHelper.tsx
+++ src/components/CourseCurriculumHelper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c748b07e1c61ed682bfab7b7c6bdfd2646be47efc8a7fab956a24a86d92a915
-size 7737
+oid sha256:cb9ba0cb19cf0f29489de653a9b89594b5e35349e9885012a44536bcc3ad3973
+size 7758
