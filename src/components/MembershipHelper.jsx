fa053a46b09abc59ed7e83e8ff537c38caf4eb52
wkenned1 <wkenned1@binghamton.edu>
2024-11-24 16:50:10 -0500

    Make membership images larger (#191)
    Resolves #182

--- src/components/MembershipHelper.jsx
+++ src/components/MembershipHelper.jsx
@@ -4,7 +4,7 @@ export default function Example(props) {
       <div className="relative isolate">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="mx-auto flex max-w-2xl flex-col gap-16 px-6 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center xl:gap-x-20 xl:px-20">
-            {props.image}
+            <div className="w-full">{props.image}</div>
             <div className="w-full flex-auto">
               <h3 className="text-3xl font-bold tracking-tight text-content sm:text-4xl">
                 Now also for teams!
