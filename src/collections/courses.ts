5b7270a98071442e623dce6de896fc9b68e09795
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2024-12-03 09:16:56 +0200

    active flag for courses

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -5,6 +5,7 @@ export default defineCollection({
   schema: ({ image }) =>
     z
       .object({
+        active: z.boolean().default(true),
         includedInMembership: z.boolean().default(true),
         archived: z.boolean().default(false),
         bundledCourses: z
