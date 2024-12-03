3eecabb5539d9b1b08a788ee2c8affc72b9a450d
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2024-12-03 08:59:32 +0200

    remove black friday

--- src/collections/courses.ts
+++ src/collections/courses.ts
@@ -40,7 +40,7 @@ export default defineCollection({
           .optional(),
         description: z
           .string()
-          .min(70, "Description must be at least 70 characters")
+          .min(20, "Description must be at least 20 characters")
           .max(500, "Description must be at most 500 characters"), // Recommended is 240 characters
         difficulty: z.enum(["beginner", "intermediate", "advanced"]).optional(),
         excerpt: z
