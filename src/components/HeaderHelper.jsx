030df04ffc4ba57da16549b4392fa29f4ff86a9c
wkenned1 <wkenned1@binghamton.edu>
2024-12-08 12:16:58 -0500

    Fix mobile header colors (#196)
    Fixes #195

--- src/components/HeaderHelper.jsx
+++ src/components/HeaderHelper.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7dc9c72ec6ce32399f380c608a12aa7d5d3d2a85b5b19f478cbce1291891925
-size 4897
+oid sha256:7b761a2f691b74480485fc0802a3bce377b9b905c4a5592cb9db6e20c97419a1
+size 4902
