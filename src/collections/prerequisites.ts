75ba2f05549fcfa8c4d07f58ff772b434dede400
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-12-11 17:42:13 +0000

    Create `prerequisites` schema

---
+++ src/collections/prerequisites.ts
@@ -0,0 +1,18 @@
+import { defineCollection, reference, z } from "astro:content";
+
+const CourseNode = z.lazy(() =>
+  z.object({
+    course: reference("courses"),
+    children: z.array(CourseNode).optional(),
+  }),
+);
+
+export default defineCollection({
+  type: "data",
+  schema: z
+    .object({
+      title: z.string(),
+      root: CourseNode,
+    })
+    .strict(),
+});
