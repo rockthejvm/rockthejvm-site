7ce2e65b0a8f5df61c12656a7a8975672590656e
wkenned1 <wkenned1@binghamton.edu>
2024-12-26 17:59:19 -0500

    Remove remaining Black Friday content (#226)
    * Remove remaining Black Friday content
    Fixes #201
    
    * Update astro.config.mjs
    
    Co-authored-by: Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
    
    * Update astro.config.mjs
    
    Co-authored-by: Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
    
    * Update Black Friday folder
    
    ---------
    
    Co-authored-by: Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2610376b0de0f353a9401a33228b80a1fbc35c0fd9a6db36e2a859b0658ddccc
-size 15902
+oid sha256:fe144be6b2423d4117a1de968612e09ae66689654e00e26760faff3b4d64220a
+size 16018
