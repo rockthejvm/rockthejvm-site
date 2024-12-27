d80684637073d5519e0c4a2cac53c8200e44ce70
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-12-27 13:04:42 +0000

    Refactor `ArticleSeriesHelper.jsx` to be compatible with React

--- src/components/ArticleSeriesHelper.jsx
+++ src/components/ArticleSeriesHelper.jsx
@@ -11,29 +11,32 @@ export default function Example({ article, articleSeries, label, open }) {
 
   return (
     <details open={open} onClick={handleClick}>
-      <summary class="cursor-pointer list-none px-4 py-5 text-content-1 sm:px-6">
-        <div class="flex flex-row justify-between">
-          <h2 class="inline max-w-full">{label ?? "Continue reading:"}</h2>{" "}
-          <p class="text-3xl">{detailsOpen ? "▼" : "►"}</p>
+      <summary className="cursor-pointer list-none px-4 py-5 text-content-1 sm:px-6">
+        <div className="flex flex-row justify-between">
+          <h2 className="inline max-w-full">{label ?? "Continue reading:"}</h2>{" "}
+          <p className="text-3xl">{detailsOpen ? "▼" : "►"}</p>
         </div>
       </summary>
-      <ol class="divide-y divide-gray-200 px-4 py-5 text-content-1 sm:p-6">
+      <ol className="divide-y divide-gray-200 px-4 py-5 text-content-1 sm:p-6">
         {articleSeries.map((seriesArticle) =>
           seriesArticle.slug === article.slug ? (
-            <li class="flex items-center py-4 text-content">
-              <span class="px-2 italic">{seriesArticle.data.title}</span>
-              <span class="text-accent-1">
+            <li
+              key={seriesArticle.slug}
+              className="flex items-center py-4 text-content"
+            >
+              <span className="px-2 italic">{seriesArticle.data.title}</span>
+              <span className="text-accent-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="mr-2 inline-block size-6 align-middle"
+                  className="mr-2 inline-block size-6 align-middle"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
                   />
                 </svg>
@@ -41,7 +44,7 @@ export default function Example({ article, articleSeries, label, open }) {
               <span>This article</span>
             </li>
           ) : (
-            <li class="py-4 text-content">
+            <li key={seriesArticle.slug} className="py-4 text-content">
               <a href={`/articles/${seriesArticle.slug}`}>
                 {seriesArticle.data.title}
               </a>
