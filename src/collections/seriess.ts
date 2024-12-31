78dc877bd5eda613a68952bf015ea399aa1e7f96
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-12-31 12:03:11 -0500

    Add article series (#217)
    * Add articles placeholder
    
    * Make description field optional in series schema
    
    * Add articles series content collections
    
    * Add ArticleSeriesTop component to display related articles
    
    * Wrap previous articles section in a WarningAdmonition component for improved visibility
    
    * Hide `ArticleSeriesTop` on first article of series
    
    * Highlight current article
    
    * Remove "You are here" label from ArticleSeries components for cleaner UI
    
    * Add underline to article titles in ArticleSeries components for improved emphasis
    
    * Combine top and bottom ArticleSeries component
    
    * Replace underline with italic styling for article titles in ArticleSeries component
    
    * Use card component
    
    * Add label and open state to ArticleSeries component for improved navigation
    
    * Add underline styling to the label in ArticleSeries component for improved emphasis
    
    * Override prose css
    
    * Remove uneccessary div
    
    * Move `AritcleLayouts` to header and footer
    
    * Remove prose utility classes from `ArticleSeries`
    
    * Update MyYouTube component to center align the video with mx-auto class
    
    * Update ArticleSeries label for clarity in ArticleLayout
    
    * Add padding to ArticleSeries section and clean up class attributes
    
    * Refactor ArticleSeries section to use dynamic background classes and improve text styling
    
    * Adjust padding and margin for improved layout in ArticleSeries component
    
    * Refactor ArticleSeries component to simplify class attributes and enhance layout
    
    * Update color variables and enhance card shadow styling in base.css
    
    * Wrap ArticleSeries component in a div for improved spacing in ArticleLayout
    
    * Have consistent gradient for both top and bottom article series component
    
    * Add label to article series component for improved accessibility
    
    * Enhance ArticleSeries component layout with flexbox for better alignment
    
    * Remove arrow
    
    * Update label in ArticleSeries component for clarity
    
    * Add NewsletterSection component for user subscription
    
    * Adjust padding in NewsletterSection component for improved spacing
    
    * Add NewsletterSection to ArticleLayout for user subscription
    
    * Fix article series component
    
    * Correct import statement for ArticleSeries component
    
    * Rename ArticleSeries component to ArticleSeriesHelper and update import references
    
    * Refactor `ArticleSeriesHelper.jsx` to be compatible with React
    
    * Update arrow and move it to the left
    
    * Update newsletter form text
    
    ---------
    
    Co-authored-by: wkenned1 <wkenned1@binghamton.edu>

--- src/collections/seriess.ts
+++ src/collections/seriess.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:101c4903e197f02e524092517a97692269d4fdc4094c74be700f86af6fd77bd0
-size 695
+oid sha256:0524beb7c28a0eb64a9c937a51eb852a84b8686db319d0f26f8e2c300d15fe67
+size 706
---
+++ src/components/ArticleSeriesHelper.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:18421a154837d4ca79df5a54ad057fcd626df9f2182ccbe7c24ea7d70b290485
+size 1994
