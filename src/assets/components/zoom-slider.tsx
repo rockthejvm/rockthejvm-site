aea119ad1ba264ada96daa6ed0357f308cecf3b7
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-01-26 23:39:33 +0000

    Fix type errors

--- src/assets/components/zoom-slider.tsx
+++ src/assets/components/zoom-slider.tsx
@@ -9,67 +9,68 @@ import { Button } from "@assets/components/ui/button";
 import { Slider } from "@assets/components/ui/slider";
 import { cn } from "@assets/lib/utils";
 
-const ZoomSlider = React.forwardRef<HTMLDivElement, Omit<"children">>(
-  ({ className, ...props }) => {
-    const { zoom } = useViewport();
-    const { zoomTo, zoomIn, zoomOut, fitView } = useReactFlow();
+const ZoomSlider = React.forwardRef<
+  HTMLDivElement,
+  Omit<React.HTMLProps<HTMLDivElement>, "children">
+>(({ className, ...props }) => {
+  const { zoom } = useViewport();
+  const { zoomTo, zoomIn, zoomOut, fitView } = useReactFlow();
 
-    const { minZoom, maxZoom } = useStore(
-      (state) => ({
-        minZoom: state.minZoom,
-        maxZoom: state.maxZoom,
-      }),
-      (a, b) => a.minZoom !== b.minZoom || a.maxZoom !== b.maxZoom,
-    );
+  const { minZoom, maxZoom } = useStore(
+    (state) => ({
+      minZoom: state.minZoom,
+      maxZoom: state.maxZoom,
+    }),
+    (a, b) => a.minZoom !== b.minZoom || a.maxZoom !== b.maxZoom,
+  );
 
-    return (
-      <Panel
-        className={cn(
-          "flex gap-1 rounded-md bg-neutral-50 p-1 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-50",
-          className,
-        )}
-        {...props}
+  return (
+    <Panel
+      className={cn(
+        "flex gap-1 rounded-md bg-neutral-50 p-1 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-50",
+        className,
+      )}
+      {...props}
+    >
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => zoomOut({ duration: 300 })}
       >
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => zoomOut({ duration: 300 })}
-        >
-          <Minus className="h-4 w-4" />
-        </Button>
-        <Slider
-          className="w-[140px]"
-          value={[zoom]}
-          min={minZoom}
-          max={maxZoom}
-          step={0.01}
-          onValueChange={(values) => zoomTo(values[0])}
-        />
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => zoomIn({ duration: 300 })}
-        >
-          <Plus className="h-4 w-4" />
-        </Button>
-        <Button
-          className="min-w-20 tabular-nums"
-          variant="ghost"
-          onClick={() => zoomTo(1, { duration: 300 })}
-        >
-          {(100 * zoom).toFixed(0)}%
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => fitView({ duration: 300 })}
-        >
-          <Maximize className="h-4 w-4" />
-        </Button>
-      </Panel>
-    );
-  },
-);
+        <Minus className="h-4 w-4" />
+      </Button>
+      <Slider
+        className="w-[140px]"
+        value={[zoom]}
+        min={minZoom}
+        max={maxZoom}
+        step={0.01}
+        onValueChange={(values) => zoomTo(values[0])}
+      />
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => zoomIn({ duration: 300 })}
+      >
+        <Plus className="h-4 w-4" />
+      </Button>
+      <Button
+        className="min-w-20 tabular-nums"
+        variant="ghost"
+        onClick={() => zoomTo(1, { duration: 300 })}
+      >
+        {(100 * zoom).toFixed(0)}%
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => fitView({ duration: 300 })}
+      >
+        <Maximize className="h-4 w-4" />
+      </Button>
+    </Panel>
+  );
+});
 
 ZoomSlider.displayName = "ZoomSlider";
 
