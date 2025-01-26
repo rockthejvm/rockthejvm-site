21992defc40877a76748488eadfd1fa1593eb65a
wkenned1 <wkenned1@binghamton.edu>
2025-01-25 20:55:06 -0500

    Cookie banner's z-axis should be adjusted (#244)
    Fixes #241

--- src/components/CookieBanner.jsx
+++ src/components/CookieBanner.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b9cd93512cc100d2bb0b08f979d85faf9a93aa08c02c5a669e630c6ddab7383
-size 2001
+oid sha256:fd5f999077738b5a95335f0b6acbeeefbc2b89bc6531b19c265223aeed597019
+size 2006
