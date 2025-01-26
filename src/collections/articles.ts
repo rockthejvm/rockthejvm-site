e2c64ac43aca36ce784bce7211cd8acd06fd307c
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-01-26 04:12:46 +0000

    Fix type errors

--- src/collections/articles.ts
+++ src/collections/articles.ts
@@ -13,7 +13,7 @@ export default defineCollection({
           .string()
           .max(200, "Description must be at most 200 characters")
           .optional(),
-git         difficulty: reference("difficulties"),
+        difficulty: reference("difficulties"),
         excerpt: z.string().refine((excerpt) => !excerpt.endsWith("."), {
           message: "Excerpt must not end with a period",
         }),
