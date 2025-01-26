938f7e6a63989c3a2a09311c5703b3cfb4fb2557
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-01-26 04:37:01 +0000

    Add Twitter share support

--- src/components/SocialShareHelper.jsx
+++ src/components/SocialShareHelper.jsx
@@ -10,7 +10,12 @@ export default function Example(props) {
   return (
     <div className="my-4 flex flex-row gap-x-3">
       <p className="my-auto italic">Share on:</p>
-      <TwitterShareButton hashtag="rockthejvm" url={props.shareUrl}>
+      <TwitterShareButton
+        title={props.title}
+        url={props.shareUrl}
+        hashtags={[...props.tags, "rockthejvm"]}
+        related={["@rockthejvm"]}
+      >
         {props.twitter}
       </TwitterShareButton>
       <FacebookShareButton hashtag="rockthejvm" url={props.shareUrl}>
