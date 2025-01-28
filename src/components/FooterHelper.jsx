1425bc4985d9501b4173a6bf79b03b8618a36d5e
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-01-28 04:19:34 +0000

    Fix linting issues

--- src/components/FooterHelper.jsx
+++ src/components/FooterHelper.jsx
@@ -264,7 +264,9 @@ export default function Example(props) {
               </div>
             </form> */}
             {props.newsletter}
-            <button onClick={clearCookies}>Clear Cookies</button>
+            <button type="button" onClick={clearCookies}>
+              Clear Cookies
+            </button>
           </div>
         </div>
         <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
