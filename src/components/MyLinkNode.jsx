c0161a2f3d9439ce430c2b86292c31e6ba72d82d
wkenned1 <wkenned1@binghamton.edu>
2025-01-30 01:58:22 +0000

    Add moving border to current course

--- src/components/MyLinkNode.jsx
+++ src/components/MyLinkNode.jsx
@@ -19,32 +19,45 @@ export default memo(({ data, isConnectable }) => {
     }
   };
 
+  const getShadowColor = (difficulty) => {
+    switch (difficulty) {
+      case "beginner":
+        return "shadow-difficulty-beginner"; // light green
+      case "intermediate":
+        return "shadow-difficulty-intermediate"; // light yellow
+      case "advanced":
+        return "shadow-difficulty-advanced"; // light red
+      default:
+        return "shadow-white"; // default white
+    }
+  };
+
   return (
-    <div
-      style={{
-        padding: "10px",
-        border: "1px solid #ddd",
-        borderRadius: "5px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        cursor: "pointer",
-        position: "relative",
-      }}
-      onClick={handleClick}
-      className={`${getBackgroundColor(data.difficulty)}`}
-    >
-      <Handle
-        type="target"
-        position={Position.Top}
-        isConnectable={isConnectable}
-      />
-      <span className="text-content">{data.label || "Clickable Node"}</span>
-      <Handle
-        type="source"
-        position={Position.Bottom}
-        isConnectable={isConnectable}
-      />
+    <div class={`${data.isCurrent ? "roadmap-node-border card" : ""}`}>
+      <div
+        style={{
+          padding: "10px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          cursor: "pointer",
+          position: "relative",
+        }}
+        onClick={handleClick}
+        className={`${getBackgroundColor(data.difficulty)} inner`}
+      >
+        <Handle
+          type="target"
+          position={Position.Top}
+          isConnectable={isConnectable}
+        />
+        <span className="text-content">{data.label || "Clickable Node"}</span>
+        <Handle
+          type="source"
+          position={Position.Bottom}
+          isConnectable={isConnectable}
+        />
+      </div>
     </div>
   );
 });
