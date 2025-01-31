c2b8ec93ba6a5c31867bf4d3eb953ddbda8cff02
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-01-31 19:37:42 +0000

    Fix some linting issues

--- src/utils/getReadingTime.ts
+++ src/utils/getReadingTime.ts
@@ -1,6 +1,6 @@
 import { fromMarkdown } from "mdast-util-from-markdown";
-import { toString } from "mdast-util-to-string";
+import * as mdast from "mdast-util-to-string";
 import calculateReadingTime from "reading-time";
 
 export default (text: string): number =>
-  Math.ceil(calculateReadingTime(toString(fromMarkdown(text))).minutes);
+  Math.ceil(calculateReadingTime(mdast.toString(fromMarkdown(text))).minutes);
