7e96eee01e8dad3dc60f8f422c35900fcab1b8e6
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-01-31 14:31:27 +0000

    Remove commented code

--- src/collections/prerequisites.ts
+++ src/collections/prerequisites.ts
@@ -8,18 +8,6 @@ const edgeSchema = z.object({
   optional: z.boolean().default(false),
 });
 
-// const baseTreeNodeSchema = z.object({
-//   value: z.string(), // Should be reference but doesn't type check
-// });
-
-// export type TreeNode = z.infer<typeof baseTreeNodeSchema> & {
-//   children: TreeNode[];
-// };
-
-// const treeNodeSchema: z.ZodType<TreeNode> = baseTreeNodeSchema.extend({
-//   children: z.lazy(() => treeNodeSchema.array()),
-// });
-
 export default defineCollection({
   type: "data",
   schema: z
