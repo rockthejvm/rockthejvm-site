f74c8cd4893759257b7759d011581904938f402a
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-02-12 19:01:58 +0000

    Swap out rules

--- .trunk/configs/.prettierrc.mjs
+++ .trunk/configs/.prettierrc.mjs
@@ -1,22 +1,32 @@
+/* trunk-ignore-all(eslint) */
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  plugins: [
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss", // must be loaded second last https://github.com/tailwindlabs/prettier-plugin-tailwindcss?tab=readme-ov-file#compatibility-with-other-prettier-plugins
+    "prettier-plugin-astro-organize-imports", // must be loaded last https://github.com/neoki07/prettier-plugin-astro-organize-imports#compatibility-with-other-prettier-plugins
+  ],
+  // General
+  printWidth: 80,
+  proseWrap: "never",
+  // Tailwind
+  tailwindStylesheet: "src/styles/global.css",
   overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
+    // Markdown
     {
       files: ["*.md", "*.mdx"],
       options: {
         embeddedLanguageFormatting: "off",
       },
     },
+    // Astro
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+        astroAllowShorthand: true,
+      },
+    },
   ],
-  astroAllowShorthand: true,
-  // astroSkipFrontmatter: true,
-  printWidth: 80,
-  proseWrap: "never",
 };
