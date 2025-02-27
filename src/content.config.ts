7c76421b0ab38a3f82c4f31efb6d08279ea461ae
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-02-27 19:58:10 +0000

    Add 404 page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,27 +1,14 @@
-import articleCategories from "@/collections/articleCategories";
-import articles from "@/collections/articles";
-import authors from "@/collections/authors";
-import companies from "@/collections/companies";
-import courseCategories from "@/collections/courseCategories";
-import courses from "@/collections/courses";
-import prerequisites from "@/collections/prerequisites";
-import testimonials from "@/pages/_testimonials/_collections/testimonials";
-import difficulties from "@/pages/difficulties/_collections/difficulties";
-import memberships from "@/pages/memberships/_collections/memberships";
-import policies from "@/pages/policies/_collections/policies";
-import tags from "@/pages/tags/_collections/tags";
-
 export const collections = {
-  articleCategories,
-  articles,
-  authors,
-  companies,
-  courseCategories,
-  courses,
-  difficulties,
-  memberships,
-  policies,
-  prerequisites,
-  tags,
-  testimonials,
+  // articleCategories,
+  // articles,
+  // authors,
+  // companies,
+  // courseCategories,
+  // courses,
+  // difficulties,
+  // memberships,
+  // policies,
+  // prerequisites,
+  // tags,
+  // testimonials,
 };
