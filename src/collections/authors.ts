7eaa65af46951441ad5de6f61baadf07a1056f4c
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-03-18 22:16:25 +0000

    temp state

--- src/collections/authors.ts
+++ src/collections/authors.ts
@@ -3,48 +3,47 @@ import { defineCollection, reference, z } from "astro:content";
 
 export default defineCollection({
   loader: glob({ pattern: "**/*.yaml", base: "src/data/authors" }),
-  schema: ({ image }) =>
-    z
-      .object({
-        biography: z.string().optional(),
-        canonicalUrl: z.string().url().optional(),
-        company: z
-          .object({
-            entity: reference("companies"),
-            role: z.string(),
-          })
-          .strict()
-          .optional(),
-        guestPlug: z.string().optional(),
-        location: z
-          .object({
-            city: z.string().optional(),
-            country: z.string(),
-          })
-          .strict()
-          .optional(),
-        name: z.string(),
-        photo: image(),
-        // .refine(
-        // 	(image) => image.width >= 460 && image.height >= 460,
-        // 	{
-        // 		message: "Photo must be at least 460x460px",
-        // 	},
-        // ),
-        socials: z
-          .object({
-            email: z.string().email().optional(),
-            facebook: handle("Facebook"),
-            github: username("GitHub"),
-            linkedin: username("LinkedIn"),
-            twitter: handle("Twitter"),
-            website: z.string().url().optional(),
-            youtube: handle("YouTube"),
-          })
-          .strict()
-          .optional(),
-      })
-      .strict(),
+  schema: z
+    .object({
+      biography: z.string().optional(),
+      canonicalUrl: z.string().url().optional(),
+      company: z
+        .object({
+          entity: reference("companies"),
+          role: z.string(),
+        })
+        .strict()
+        .optional(),
+      guestPlug: z.string().optional(),
+      location: z
+        .object({
+          city: z.string().optional(),
+          country: z.string(),
+        })
+        .strict()
+        .optional(),
+      name: z.string(),
+      photo: z.string(),
+      // .refine(
+      // 	(image) => image.width >= 460 && image.height >= 460,
+      // 	{
+      // 		message: "Photo must be at least 460x460px",
+      // 	},
+      // ),
+      socials: z
+        .object({
+          email: z.string().email().optional(),
+          facebook: handle("Facebook"),
+          github: username("GitHub"),
+          linkedin: username("LinkedIn"),
+          twitter: handle("Twitter"),
+          website: z.string().url().optional(),
+          youtube: handle("YouTube"),
+        })
+        .strict()
+        .optional(),
+    })
+    .strict(),
 });
 
 const username = (field: string) =>
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,20 @@
 import policies from "@/pages/policies/_collections/policies";
+import articles from "./collections/articles";
+import authors from "./collections/authors";
+import difficulties from "./pages/difficulties/_collections/difficulties";
+import tags from "./pages/tags/_collections/tags";
 
 export const collections = {
   // articleCategories,
-  // articles,
-  // authors,
+  articles,
+  authors,
   // companies,
   // courseCategories,
   // courses,
-  // difficulties,
+  difficulties,
   // memberships,
   policies,
   // prerequisites,
-  // tags,
+  tags,
   // testimonials,
 };
