7c3bf57ccf5f8d045497e4f429b0177e5b820269
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-03-19 00:53:09 +0000

    Get content collections revolving around article setup

--- src/collections/authors.ts
+++ src/collections/authors.ts
@@ -1,51 +1,6 @@
 import { glob } from "astro/loaders";
 import { defineCollection, reference, z } from "astro:content";
 
-export default defineCollection({
-  loader: glob({ pattern: "**/*.yaml", base: "src/data/authors" }),
-  schema: z
-    .object({
-      biography: z.string().optional(),
-      canonicalUrl: z.string().url().optional(),
-      company: z
-        .object({
-          entity: reference("companies"),
-          role: z.string(),
-        })
-        .strict()
-        .optional(),
-      guestPlug: z.string().optional(),
-      location: z
-        .object({
-          city: z.string().optional(),
-          country: z.string(),
-        })
-        .strict()
-        .optional(),
-      name: z.string(),
-      photo: z.string(),
-      // .refine(
-      // 	(image) => image.width >= 460 && image.height >= 460,
-      // 	{
-      // 		message: "Photo must be at least 460x460px",
-      // 	},
-      // ),
-      socials: z
-        .object({
-          email: z.string().email().optional(),
-          facebook: handle("Facebook"),
-          github: username("GitHub"),
-          linkedin: username("LinkedIn"),
-          twitter: handle("Twitter"),
-          website: z.string().url().optional(),
-          youtube: handle("YouTube"),
-        })
-        .strict()
-        .optional(),
-    })
-    .strict(),
-});
-
 const username = (field: string) =>
     z
       .string()
@@ -59,3 +14,49 @@ const username = (field: string) =>
       .string()
       .regex(/^@/, `${field} should start with the '@' symbol`)
       .optional();
+
+export default defineCollection({
+  loader: glob({ pattern: "**/*.yaml", base: "src/data/authors" }),
+  schema: ({ image }) =>
+    z
+      .object({
+        biography: z.string().optional(),
+        canonicalUrl: z.string().url().optional(),
+        company: z
+          .object({
+            entity: reference("companies"),
+            role: z.string(),
+          })
+          .strict()
+          .optional(),
+        guestPlug: z.string().optional(),
+        location: z
+          .object({
+            city: z.string().optional(),
+            country: z.string(),
+          })
+          .strict()
+          .optional(),
+        name: z.string(),
+        photo: image(),
+        // .refine(
+        // 	(image) => image.width >= 460 && image.height >= 460,
+        // 	{
+        // 		message: "Photo must be at least 460x460px",
+        // 	},
+        // ),
+        socials: z
+          .object({
+            email: z.string().email().optional(),
+            facebook: handle("Facebook"),
+            github: username("GitHub"),
+            linkedin: username("LinkedIn"),
+            twitter: handle("Twitter"),
+            website: z.string().url().optional(),
+            youtube: handle("YouTube"),
+          })
+          .strict()
+          .optional(),
+      })
+      .strict(),
+});
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,16 @@
 import policies from "@/pages/policies/_collections/policies";
+import articleCategories from "./collections/articleCategories";
 import articles from "./collections/articles";
 import authors from "./collections/authors";
+import companies from "./collections/companies";
 import difficulties from "./pages/difficulties/_collections/difficulties";
 import tags from "./pages/tags/_collections/tags";
 
 export const collections = {
-  // articleCategories,
+  articleCategories,
   articles,
   authors,
-  // companies,
+  companies,
   // courseCategories,
   // courses,
   difficulties,
