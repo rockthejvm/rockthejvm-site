cd20ff4fbbf6921ec4fbafaa95659cebe09301a0
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2025-03-27 20:38:35 +0000

    Fetch price on build time

---
+++ src/utils/centsToPrice.ts
@@ -0,0 +1,7 @@
+export default (cents: number, currency: string): string =>
+  (cents / 100).toLocaleString("en-US", {
+    style: "currency",
+    currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
