cd7d058c1701dfcfcd9c1ea22d2f224f42c017dc
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2026-04-12 12:54:57 +0300

    author page with courses

--- src/components/AuthorWideHelper.jsx
+++ src/components/AuthorWideHelper.jsx
@@ -1,12 +1,12 @@
 export default function Example(props) {
   return (
-    <div className="my-auto py-4 sm:flex">
+    <div className="my-auto py-4 px-8 sm:flex">
       <div className="mb-4 shrink-0 sm:mb-0 sm:mr-4">{props.photo}</div>
       <div className="my-auto">
         <a href={`/authors/${props.authorId}`}>
-          <p className="text-2xl font-bold text-content-2 hover:text-accent-2 sm:text-xl md:text-2xl lg:text-3xl">
+          <h1 className="text-2xl font-bold text-content-2 hover:text-accent-2 sm:text-xl md:text-2xl lg:text-3xl">
             {props.name}
-          </p>
+          </h1>
         </a>
         {props.biography && (
           <p className="mt-1 text-lg text-content-2 sm:text-base md:text-lg lg:text-xl">
